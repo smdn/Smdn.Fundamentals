127ac81f2b59d315fb958a4d9f1ae9abe7ff9b54
smdn <smdn@smdn.jp>
2009-08-30 03:13:48 +0900

    * PathUtils.cs: added test cases

--- Smdn/Test/Smdn.IO/PathUtils.cs
+++ Smdn/Test/Smdn.IO/PathUtils.cs
@@ -5,23 +5,58 @@ namespace Smdn.IO {
   [TestFixture]
   public class PathUtilsTests {
     [Test]
-    public void TestEquals()
+    public void TestArePathEqual()
     {
       if (Runtime.IsRunningOnUnix) {
-        Assert.IsTrue(PathUtils.Equals("/var/log/", "/var/log/"));
-        Assert.IsTrue(PathUtils.Equals("/var/log/", "/var/log"));
-        Assert.IsFalse(PathUtils.Equals("/var/log/", "/var/Log/"));
-        Assert.IsFalse(PathUtils.Equals("/var/log/", "/var/Log"));
+        Assert.IsTrue(PathUtils.ArePathEqual("/var/log/", "/var/log/"));
+        Assert.IsTrue(PathUtils.ArePathEqual("/var/log/", "/var/log"));
+        Assert.IsFalse(PathUtils.ArePathEqual("/var/log/", "/var/Log/"));
+        Assert.IsFalse(PathUtils.ArePathEqual("/var/log/", "/var/Log"));
       }
       else {
-        Assert.IsTrue(PathUtils.Equals(@"C:\Windows\", @"C:\Windows\"));
-        Assert.IsTrue(PathUtils.Equals(@"C:\Windows\", @"C:\Windows"));
-        Assert.IsTrue(PathUtils.Equals(@"C:\Windows\", @"C:\windows\"));
-        Assert.IsTrue(PathUtils.Equals(@"C:\Windows\", @"C:\windows"));
-        Assert.IsTrue(PathUtils.Equals(@"C:/Windows/", @"C:/Windows/"));
-        Assert.IsTrue(PathUtils.Equals(@"C:/Windows/", @"C:/Windows"));
-        Assert.IsTrue(PathUtils.Equals(@"C:/Windows/", @"C:/windows/"));
-        Assert.IsTrue(PathUtils.Equals(@"C:/Windows/", @"C:/windows"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:\Windows\", @"C:\Windows\"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:\Windows\", @"C:\Windows"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:\Windows\", @"C:\windows\"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:\Windows\", @"C:\windows"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:/Windows/", @"C:/Windows/"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:/Windows/", @"C:/Windows"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:/Windows/", @"C:/windows/"));
+        Assert.IsTrue(PathUtils.ArePathEqual(@"C:/Windows/", @"C:/windows"));
+      }
+    }
+
+    [Test]
+    public void TestAreExtensionEqual()
+    {
+      if (Runtime.IsRunningOnUnix) {
+        Assert.IsTrue(PathUtils.AreExtensionEqual("/etc/conf.ini", ".ini"));
+        Assert.IsFalse(PathUtils.AreExtensionEqual("/etc/CONF.INI", ".ini"));
+        Assert.IsFalse(PathUtils.AreExtensionEqual("/etc/Conf.Ini", ".ini"));
+
+        Assert.IsFalse(PathUtils.AreExtensionEqual("/etc/conf.ini", ".txt"));
+
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"test.jpeg", "test.jpeg"));
+        Assert.IsFalse(PathUtils.AreExtensionEqual(@"test.jpeg", "TEST.JPEG"));
+        Assert.IsFalse(PathUtils.AreExtensionEqual(@"test.jpeg", "Test.Jpeg"));
+
+        Assert.IsFalse(PathUtils.AreExtensionEqual(@"test.jpeg", "test.png"));
+      }
+      else {
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"C:\WINDOWS\boot.ini", ".ini"));
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"C:\WINDOWS\BOOT.INI", ".ini"));
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"C:\WINDOWS\Boot.Ini", ".ini"));
+
+        Assert.IsFalse(PathUtils.AreExtensionEqual(@"C:\WINDOWS\boot.ini", ".txt"));
+
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"C:\WINDOWS\boot.ini", ".ini"));
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"C:\WINDOWS\boot.ini", ".INI"));
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"C:\WINDOWS\boot.ini", ".Ini"));
+
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"test.jpeg", "test.jpeg"));
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"test.jpeg", "TEST.JPEG"));
+        Assert.IsTrue(PathUtils.AreExtensionEqual(@"test.jpeg", "Test.Jpeg"));
+
+        Assert.IsFalse(PathUtils.AreExtensionEqual(@"test.jpeg", "test.png"));
       }
     }
   }
