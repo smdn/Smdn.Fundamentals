4306705e84021369f17a115552814443c988bbe5
smdn <smdn@smdn.jp>
2009-09-01 15:54:42 +0900

    moved Smdn.Windows.Forms/FileDialogFilter.cs to Smdn.IO/FileDialogFilter.cs

--- Smdn/Test/Smdn.IO/FileDialogFilter.cs
+++ Smdn/Test/Smdn.IO/FileDialogFilter.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
-namespace Smdn.Windows.Forms {
+namespace Smdn.IO {
   [TestFixture]
   public class FileDialogFilterTest {
     [Test]
