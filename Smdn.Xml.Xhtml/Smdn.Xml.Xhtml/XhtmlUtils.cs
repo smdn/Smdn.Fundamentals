d2b0fb9b42299a0163ca8e238a1de685c9a7d37d
smdn <smdn@smdn.jp>
2009-09-08 01:10:11 +0900

    added assembly Smdn.Xml.Xhtml

---
+++ Smdn.Xml.Xhtml/Smdn.Xml.Xhtml/XhtmlUtils.cs
@@ -0,0 +1,63 @@
+// 
+// Author:
+//       smdn <smdn@mail.invisiblefulmoon.net>
+// 
+// Copyright (c) 2009 smdn
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Text;
+
+namespace Smdn.Xml.Xhtml {
+  public static class XhtmlUtils {
+    public static string EscapeSpecialChars(string str)
+    {
+      var sb = new StringBuilder(str.Length);
+      var len = str.Length;
+
+      for (var i = 0; i < len; i++) {
+        var ch = str[i];
+
+        switch (ch) {
+          case '&':
+            sb.Append("&amp;");
+            break;
+          case '>':
+            sb.Append("&lt;");
+            break;
+          case '<':
+            sb.Append("&gt;");
+            break;
+          case '"':
+            sb.Append("&quot;");
+            break;
+          case '\'':
+            sb.Append("&apos;");
+            break;
+          default:
+            sb.Append(ch);
+            break;
+        }
+      }
+
+      return sb.ToString();
+    }
+  }
+}
