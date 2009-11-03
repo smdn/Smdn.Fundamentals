31c382b8442fa5ba71080ce5708094c930535135
smdn <smdn@smdn.jp>
2009-11-03 20:24:40 +0900

    * PathUtils.cs: added test cases

--- Smdn/Test/Smdn.IO/PathUtils.cs
+++ Smdn/Test/Smdn.IO/PathUtils.cs
@@ -94,5 +94,51 @@ namespace Smdn.IO {
         Assert.IsFalse(PathUtils.AreExtensionEqual(@"test.jpeg", "test.png"));
       }
     }
+
+    [Test]
+    public void TestContainsShellEscapeChar()
+    {
+      var shift_jis = System.Text.Encoding.GetEncoding(932);
+      var ngchars = "―ソЫⅨ噂浬欺圭構蚕十申曾箪貼能表暴予禄兔喀媾彌拿杤歃濬畚秉綵臀藹觸軆鐔饅鷭"; // XXX: "偆砡纊犾"
+
+      foreach (var c in ngchars.ToCharArray()) {
+        var s = c.ToString();
+
+        Assert.IsTrue(PathUtils.ContainsShellEscapeChar(s, shift_jis),
+                      string.Format("char: {0}, bytes: {1}", s, BitConverter.ToString(shift_jis.GetBytes(s))));
+      }
+
+      Assert.IsTrue(PathUtils.ContainsShellEscapeChar("六十年", shift_jis));
+      Assert.IsTrue(PathUtils.ContainsShellEscapeChar("明治十七年の上海アリス", shift_jis));
+    }
+
+    [Test]
+    public void TestContainsShellPipeChar()
+    {
+      var shift_jis = System.Text.Encoding.GetEncoding(932);
+      var ngchars = "ポл榎掛弓芸鋼旨楯酢竹倒培怖翻慾處嘶斈忿掟桍毫烟痞窩縹艚蛞諫轎閖驂黥"; // XXX: 埈蒴僴礰
+
+      foreach (var c in ngchars.ToCharArray()) {
+        var s = c.ToString();
+
+        Assert.IsTrue(PathUtils.ContainsShellPipeChar(s, shift_jis),
+                      string.Format("char: {0}, bytes: {1}", s, BitConverter.ToString(shift_jis.GetBytes(s))));
+      }
+
+      Assert.IsTrue(PathUtils.ContainsShellPipeChar("竹取物語", shift_jis));
+      Assert.IsTrue(PathUtils.ContainsShellPipeChar("ポケモン", shift_jis));
+    }
+
+    [Test]
+    public void TestContainsShellSpecialChars()
+    {
+      var shift_jis = System.Text.Encoding.GetEncoding(932);
+      var ngchars = new byte[] {0x5c, 0x7c};
+
+      Assert.IsTrue(PathUtils.ContainsShellSpecialChars("六十年", shift_jis, ngchars));
+      Assert.IsTrue(PathUtils.ContainsShellSpecialChars("明治十七年の上海アリス", shift_jis, ngchars));
+      Assert.IsTrue(PathUtils.ContainsShellSpecialChars("竹取物語", shift_jis, ngchars));
+      Assert.IsTrue(PathUtils.ContainsShellSpecialChars("ポケモン", shift_jis, ngchars));
+    }
   }
 }
