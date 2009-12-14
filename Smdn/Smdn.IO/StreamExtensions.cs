8d3e635b71c4e9bb5357b0e3fdaa19b408c8539a
smdn <smdn@smdn.jp>
2009-12-14 17:44:59 +0900

    * Smdn.csproj: * Test/Smdn.Tests.csproj: * Smdn.IO/StreamExtensions.cs: * Test/Smdn.IO/StreamExtensions.cs: added StreamExtensions.WriteToEnd

---
+++ Smdn/Smdn.IO/StreamExtensions.cs
@@ -0,0 +1,54 @@
+// 
+// Author:
+//       smdn <smdn@mail.invisiblefulmoon.net>
+// 
+// Copyright (c) 2009 smdn
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+
+namespace Smdn.IO {
+  public static class StreamExtensions {
+    public static void WriteToEnd(this Stream fromStream, Stream toStream)
+    {
+      WriteToEnd(fromStream, toStream, 4096);
+    }
+
+    public static void WriteToEnd(this Stream fromStream, Stream toStream, int bufferSize)
+    {
+      if (toStream == null)
+        throw new ArgumentNullException("toStream");
+      if (bufferSize <= 0)
+        throw new ArgumentOutOfRangeException("bufferSize", "must be non-zero positive number");
+
+      var buffer = new byte[bufferSize];
+
+      for (;;) {
+        var read = fromStream.Read(buffer, 0, bufferSize);
+
+        if (read <= 0)
+          break;
+
+        toStream.Write(buffer, 0, read);
+      }
+    }
+  }
+}
---
+++ Smdn/Test/Smdn.IO/StreamExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+using Smdn.Formats;
+
+namespace Smdn.IO {
+  [TestFixture]
+  public class StreamExtensionsTests {
+    [Test]
+    public void TestWriteToEndStreamGreaterThanBufferSize()
+    {
+      var inputData = new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+      var inputStream = new MemoryStream(inputData);
+      var outputStream = new MemoryStream();
+
+      StreamExtensions.WriteToEnd(inputStream, outputStream, 3);
+
+      Assert.AreEqual(8, outputStream.Length);
+
+      outputStream.Close();
+
+      Assert.AreEqual(inputData, outputStream.ToArray());
+    }
+
+    [Test]
+    public void TestWriteToEndStreamLessThanBufferSize()
+    {
+      var inputData = new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+      var inputStream = new MemoryStream(inputData);
+      var outputStream = new MemoryStream();
+
+      StreamExtensions.WriteToEnd(inputStream, outputStream, 16);
+
+      Assert.AreEqual(8, outputStream.Length);
+
+      outputStream.Close();
+
+      Assert.AreEqual(inputData, outputStream.ToArray());
+    }
+
+    [Test]
+    public void TestWriteToEndPositionedStream()
+    {
+      var inputData = new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+      var inputStream = new MemoryStream(inputData);
+      var outputStream = new MemoryStream();
+
+      inputStream.Seek(4, SeekOrigin.Begin);
+      outputStream.Seek(8, SeekOrigin.Begin);
+
+      StreamExtensions.WriteToEnd(inputStream, outputStream);
+
+      Assert.AreEqual(12, outputStream.Length);
+
+      outputStream.Close();
+
+      Assert.AreEqual(new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x05, 0x06, 0x07}, outputStream.ToArray());
+    }
+  }
+}
