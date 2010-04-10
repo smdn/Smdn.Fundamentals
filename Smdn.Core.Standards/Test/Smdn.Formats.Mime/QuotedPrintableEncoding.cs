6a08ab22d50c0a270cbecb75e04a7cfc63690a27
smdn <smdn@smdn.jp>
2010-04-10 19:39:15 +0900

    * Smdn.Formats.Mime: * Smdn.Formats/MimeEncoding.cs: * Smdn.Core.Standards.Tests.csproj: * Smdn.Formats.Mime/MimeEncoding.cs: * Smdn.Formats/QuotedPrintableEncoding.cs: * Smdn.Formats.Mime/QuotedPrintableEncoding.cs: renamed namespace

--- Smdn.Core.Standards/Test/Smdn.Formats.Mime/QuotedPrintableEncoding.cs
+++ Smdn.Core.Standards/Test/Smdn.Formats.Mime/QuotedPrintableEncoding.cs
@@ -2,7 +2,7 @@ using System;
 using System.Text;
 using NUnit.Framework;
 
-namespace Smdn.Formats {
+namespace Smdn.Formats.Mime {
   [TestFixture]
   public class QuotedPrintableEncodingTests {
     [Test]
