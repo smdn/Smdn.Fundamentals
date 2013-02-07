9fbbaeec3f26b7cc2f455eaee9b51fb0d4c57d93
smdn <smdn@smdn.jp>
2013-02-07 21:27:57 +0900

    updated copyright

--- Smdn.Core.Standards/Smdn.Formats.Mime/QuotedPrintableEncoding.cs
+++ Smdn.Core.Standards/Smdn.Formats.Mime/QuotedPrintableEncoding.cs
@@ -2,7 +2,7 @@
 // Author:
 //       smdn <smdn@smdn.jp>
 // 
-// Copyright (c) 2010-2012 smdn
+// Copyright (c) 2010-2013 smdn
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
