baa033763f1411b8439d13f21ee5c05d21c956fe
smdn <smdn@smdn.jp>
2013-05-20 23:28:43 +0900

    removed test code

--- Smdn/Test/Smdn/EmptyArray.cs
+++ Smdn/Test/Smdn/EmptyArray.cs
@@ -12,27 +12,5 @@ namespace Smdn {
       Assert.AreEqual(0, intArray.Length);
       Assert.AreSame(intArray, EmptyArray<int>.Instance);
     }
-
-#if false
-    [Test]
-    public void TRest()
-    {
-      const int eee = 10000000;
-
-      var sw1 = System.Diagnostics.Stopwatch.StartNew();
-      for (var i = 0; i < eee; i++) {
-        foreach (var e in new int[0]) {
-        }
-      }
-      Console.WriteLine(sw1.Elapsed);
-      var sw2 = System.Diagnostics.Stopwatch.StartNew();
-      for (var i = 0; i < eee; i++) {
-        foreach (var e in EmptyArray<int>.Instance) {
-        }
-      }
-      Console.WriteLine(sw2.Elapsed);
-      Assert.Fail();
-    }
-#endif
   }
 }
