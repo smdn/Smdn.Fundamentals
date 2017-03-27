5a73a44e9fef0720abd6034d583e536b0855bf5e
smdn <smdn@smdn.jp>
2017-03-27 18:03:29 +0900

    redirected implementation of Smdn.EnvironmentUtils

--- Smdn/Smdn/EnvironmentUtils.cs
+++ Smdn/Smdn/EnvironmentUtils.cs
@@ -28,52 +28,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Smdn {
+  [Obsolete("use Smdn.OperatingSystem.EnvironmentUtils instead")]
   public static class EnvironmentUtils {
     public static Dictionary<string, string> ParseEnvironmentVariables(string variables)
     {
-      return ParseEnvironmentVariables(variables, true);
+      return Smdn.OperatingSystem.EnvironmentUtils.ParseEnvironmentVariables(variables);
     }
 
     public static Dictionary<string, string> ParseEnvironmentVariables(string variables, bool throwIfInvalid)
     {
-      if (variables == null)
-        return null;
-
-      var ret = new Dictionary<string, string>();
-
-      foreach (var pair in variables.Split(new[] {Path.PathSeparator}, StringSplitOptions.RemoveEmptyEntries)) {
-        var delim = pair.IndexOf('=');
-
-        if (delim < 0) {
-          if (throwIfInvalid)
-            throw new FormatException("invalid format");
-          else
-            continue;
-        }
-
-        ret.Add(pair.Substring(0, delim).Trim(), pair.Substring(delim + 1));
-      }
-
-      return ret;
+      return Smdn.OperatingSystem.EnvironmentUtils.ParseEnvironmentVariables(variables, throwIfInvalid);
     }
 
     public static string CombineEnvironmentVariables(IDictionary<string, string> variables)
     {
-      if (variables == null)
-        return null;
-
-      var ret = new StringBuilder();
-
-      foreach (var pair in variables) {
-        if (0 < ret.Length)
-          ret.Append(Path.PathSeparator);
-
-        ret.Append(pair.Key);
-        ret.Append('=');
-        ret.Append(pair.Value);
-      }
-
-      return ret.ToString();
+      return Smdn.OperatingSystem.EnvironmentUtils.CombineEnvironmentVariables(variables);
     }
   }
 }
