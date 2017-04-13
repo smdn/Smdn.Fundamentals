8ef82a0d791f83698a90b2d4fe2e2bb962093c57
smdn <smdn@smdn.jp>
2017-04-13 20:12:15 +0900

    added mock class of (To|From)Base64Transform

---
+++ Smdn/src/Smdn.Security.Cryptography/ToBase64Transform.cs
@@ -0,0 +1,58 @@
+﻿// 
+// Author:
+//       smdn <smdn@smdn.jp>
+// 
+// Copyright (c) 2017 smdn
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Security.Cryptography;
+
+namespace Smdn.Security.Cryptography {
+  public class ToBase64Transform : ICryptoTransform {
+    public bool CanReuseTransform => throw new NotImplementedException();
+
+    public bool CanTransformMultipleBlocks => throw new NotImplementedException();
+
+    public int InputBlockSize => throw new NotImplementedException();
+
+    public int OutputBlockSize => throw new NotImplementedException();
+
+    public ToBase64Transform()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+
+    public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+    {
+      throw new NotImplementedException();
+    }
+
+    public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
