8f949df9c84fe148fbd915b18c9b052ac2f723eb
smdn <smdn@smdn.jp>
2017-04-18 20:23:00 +0900

    added tests for (From|To)Base64Transform

---
+++ Smdn/test/Smdn.Security.Cryptography/FromBase64Transform.cs
@@ -0,0 +1,312 @@
+using System;
+using System.Text;
+#if NET46
+using System.Security.Cryptography;
+#else
+using Smdn.Security.Cryptography;
+#endif
+using NUnit.Framework;
+
+namespace Smdn.Security.Cryptography {
+  [TestFixture]
+  public class FromBase64TransformTests {
+    [Test]
+    public void TestProperties()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.IsTrue(t.CanReuseTransform);
+        Assert.IsFalse(t.CanTransformMultipleBlocks);
+        Assert.AreEqual(1, t.InputBlockSize);
+        Assert.AreEqual(3, t.OutputBlockSize);
+      }
+    }
+
+    [Test]
+    public void TestDispose()
+    {
+      using (var t = new FromBase64Transform()) {
+        t.Dispose();
+
+        var input = new byte[1];
+        var output = new byte[3];
+
+        Assert.Throws<ObjectDisposedException>(() => t.TransformBlock(input, 0, input.Length, output, 0));
+      }
+    }
+
+    [Test]
+    public void TestClear()
+    {
+      using (var t = new FromBase64Transform()) {
+        t.Clear();
+
+        var input = new byte[1];
+        var output = new byte[3];
+
+        Assert.Throws<ObjectDisposedException>(() => t.TransformBlock(input, 0, input.Length, output, 0));
+      }
+    }
+
+#if false
+      foreach (var pattern in new[] {
+        new {Output = "A",     Input = "QQ=="},
+        new {Output = "AS",    Input = "QVM="},
+        new {Output = "ASC",   Input = "QVND"},
+        new {Output = "ASCI",  Input = "QVNDSQ=="},
+        new {Output = "ASCII", Input = "QVNDSQk="},
+      }) {
+      }
+#endif
+
+    [Test]
+    public void TestTransformBlock()
+    {
+      foreach (var pattern in new[] {
+        new {Output = "A",     Input = "QQ=="},
+        new {Output = "AS",    Input = "QVM="},
+        new {Output = "ASC",   Input = "QVND"},
+        new {Output = "ASCI",  Input = "QVNDSQ=="},
+        new {Output = "ASCII", Input = "QVNDSUk="},
+      }) {
+        using (var t = new FromBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[8];
+
+          var transformedLength = t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0);
+
+          Assert.AreEqual(pattern.Output.Length, transformedLength);
+          Assert.AreEqual(pattern.Output, Encoding.ASCII.GetString(outputBuffer, 0, transformedLength));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_IgnoreWhiteSpaces()
+    {
+      foreach (var pattern in new[] {
+        new {Output = "ASCII", Input = " QVNDSUk="},
+        new {Output = "ASCII", Input = "QVNDSUk= "},
+        new {Output = "ASCII", Input = "QVND SUk="},
+        new {Output = "ASCII", Input = "QVN DSUk="},
+        new {Output = "ASCII", Input = "QVNDS Uk="},
+        new {Output = "ASCII", Input = "QVND\tSUk="},
+        new {Output = "ASCII", Input = "QVND\nSUk="},
+        new {Output = "ASCII", Input = "QVND\rSUk="},
+        new {Output = "ASCII", Input = "QVND\r\nSUk="},
+      }) {
+        using (var t = new FromBase64Transform(FromBase64TransformMode.IgnoreWhiteSpaces)) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[8];
+
+          var transformedLength = t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0);
+
+          Assert.AreEqual(pattern.Output.Length, transformedLength);
+          Assert.AreEqual(pattern.Output, Encoding.ASCII.GetString(outputBuffer, 0, transformedLength));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_DoNotIgnoreWhiteSpaces()
+    {
+      foreach (var pattern in new[] {
+        new {Output = "ASCII", Input = " QVNDSUk="},
+//        new {Output = "ASCII", Input = "QVNDSUk= "},
+        new {Output = "ASCII", Input = "QVND SUk="},
+        new {Output = "ASCII", Input = "QVN DSUk="},
+        new {Output = "ASCII", Input = "QVNDS Uk="},
+        new {Output = "ASCII", Input = "QVND\tSUk="},
+        new {Output = "ASCII", Input = "QVND\nSUk="},
+        new {Output = "ASCII", Input = "QVND\rSUk="},
+        new {Output = "ASCII", Input = "QVND\r\nSUk="},
+      }) {
+        using (var t = new FromBase64Transform(FromBase64TransformMode.DoNotIgnoreWhiteSpaces)) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[8];
+
+          Assert.Throws<FormatException>(() => t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0), "input = " + pattern.Input);
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_DoNotIgnoreWhiteSpaces_IgnoresTrailingWhiteSpaces()
+    {
+      foreach (var pattern in new[] {
+        new {Output = "A",     Input = "QQ== "},
+        new {Output = "AS",    Input = "QVM= "},
+        new {Output = "ASC",   Input = "QVND "},
+        new {Output = "ASCI",  Input = "QVNDSQ== "},
+        new {Output = "ASCII", Input = "QVNDSUk= "},
+        new {Output = "ASCII", Input = "QVNDSUk=\t"},
+        new {Output = "ASCII", Input = "QVNDSUk=\r"},
+        new {Output = "ASCII", Input = "QVNDSUk=\n"},
+        new {Output = "ASCII", Input = "QVNDSUk=\r\n"},
+      }) {
+        using (var t = new FromBase64Transform(FromBase64TransformMode.DoNotIgnoreWhiteSpaces)) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[8];
+
+          var transformedLength = t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0);
+
+          Assert.AreEqual(pattern.Output.Length, transformedLength);
+          Assert.AreEqual(pattern.Output, Encoding.ASCII.GetString(outputBuffer, 0, transformedLength));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferShorterThanBlockSize()
+    {
+      foreach (var pattern in new[] {
+        new {Output = "A",     Input = ""},
+        new {Output = "A",     Input = "Q"},
+        new {Output = "A",     Input = "QQ"},
+        new {Output = "A",     Input = "QQ="},
+      }) {
+        using (var t = new FromBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[3];
+
+          var length = t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0);
+
+          Assert.AreEqual(0, length);
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferNull()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentNullException>(() => t.TransformBlock(null, 0, 1, new byte[3], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferInvalidLength()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[1], 0, 2, new byte[3], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferInvalidOffset()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[1], 1, 1, new byte[3], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferRangeNegative()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformBlock(new byte[1], -1, 1, new byte[3], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferRangeOverflow()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[1], 1, int.MaxValue, new byte[3], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_OutputBufferNull()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentNullException>(() => t.TransformBlock(new byte[4] { 0x41, 0x41, 0x41, 0x41 }, 0, 4, null, 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_OutputBufferInvalidOffset()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[4] { 0x41, 0x41, 0x41, 0x41 }, 0, 4, new byte[3], 4));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_OutputBufferRangeNegative()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformBlock(new byte[4] { 0x41, 0x41, 0x41, 0x41 }, 0, 4, new byte[3], -1));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock()
+    {
+      foreach (var pattern in new[] {
+        new {Output = "A",     Input = "QQ=="},
+        new {Output = "AS",    Input = "QVM="},
+        new {Output = "ASC",   Input = "QVND"},
+        new {Output = "ASCI",  Input = "QVNDSQ=="},
+        new {Output = "ASCII", Input = "QVNDSUk="},
+      }) {
+        using (var t = new FromBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[8];
+
+          var ret = t.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+
+          Assert.AreEqual(pattern.Output.Length, ret.Length);
+          Assert.AreEqual(pattern.Output, Encoding.ASCII.GetString(ret));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferEmpty()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.IsEmpty(t.TransformFinalBlock(new byte[0], 0, 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferNull()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentNullException>(() => t.TransformFinalBlock(null, 0, 1));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferInvalidLength()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformFinalBlock(new byte[1], 0, 2));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferInvalidOffset()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformFinalBlock(new byte[1], 1, 1));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferRangeNegative()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformFinalBlock(new byte[1], -1, 1));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferRangeOverflow()
+    {
+      using (var t = new FromBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformFinalBlock(new byte[1], 1, int.MaxValue));
+      }
+    }
+  }
+}
---
+++ Smdn/test/Smdn.Security.Cryptography/ToBase64Transform.cs
@@ -0,0 +1,243 @@
+using System;
+using System.Text;
+#if NET46
+using System.Security.Cryptography;
+#else
+using Smdn.Security.Cryptography;
+#endif
+using NUnit.Framework;
+
+namespace Smdn.Security.Cryptography {
+  [TestFixture]
+  public class ToBase64TransformTests {
+    [Test]
+    public void TestProperties()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.IsTrue(t.CanReuseTransform);
+        Assert.IsFalse(t.CanTransformMultipleBlocks);
+        Assert.AreEqual(3, t.InputBlockSize);
+        Assert.AreEqual(4, t.OutputBlockSize);
+      }
+    }
+
+    [Test]
+    public void TestDispose()
+    {
+      using (var t = new ToBase64Transform()) {
+        t.Dispose();
+
+        var input = new byte[3];
+        var output = new byte[4];
+
+        Assert.DoesNotThrow(() => t.TransformBlock(input, 0, input.Length, output, 0));
+      }
+    }
+
+    [Test]
+    public void TestClear()
+    {
+      using (var t = new ToBase64Transform()) {
+        t.Clear();
+
+        var input = new byte[3];
+        var output = new byte[4];
+
+        Assert.DoesNotThrow(() => t.TransformBlock(input, 0, input.Length, output, 0));
+      }
+    }
+
+#if false
+      foreach (var pattern in new[] {
+        new {Input = "A",     Output = "QQ=="},
+        new {Input = "AS",    Output = "QVM="},
+        new {Input = "ASC",   Output = "QVND"},
+        new {Input = "ASCI",  Output = "QVNDSQ=="},
+        new {Input = "ASCII", Output = "QVNDSUk="},
+      }) {
+      }
+#endif
+
+    [Test]
+    public void TestTransformBlock()
+    {
+      foreach (var pattern in new[] {
+        new {Input = "ASC",   Output = "QVND"},
+        new {Input = "ASCI",  Output = "QVND"},
+        new {Input = "ASCII", Output = "QVND"},
+      }) {
+        using (var t = new ToBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[4];
+
+          var transformedLength = t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0);
+
+          Assert.AreEqual(4, transformedLength);
+          Assert.AreEqual(pattern.Output, Encoding.ASCII.GetString(outputBuffer, 0, transformedLength));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferTooShort()
+    {
+      foreach (var pattern in new[] {
+        new {Input = "A",     Output = "QQ=="},
+        new {Input = "AS",    Output = "QVM="},
+      }) {
+        using (var t = new ToBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+          var outputBuffer = new byte[4];
+
+          Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformBlock(inputBuffer, 0, inputBuffer.Length, outputBuffer, 0));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferNull()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentNullException>(() => t.TransformBlock(null, 0, 3, new byte[4], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferInvalidLength()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[3], 0, 4, new byte[4], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferInvalidOffset()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[3], 1, 3, new byte[4], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferRangeNegative()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformBlock(new byte[3], -1, 3, new byte[4], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_InputBufferRangeOverflow()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[3], 1, int.MaxValue, new byte[4], 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_OutputBufferNull()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentNullException>(() => t.TransformBlock(new byte[3], 0, 3, null, 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_OutputBufferInvalidOffset()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformBlock(new byte[3], 0, 3, new byte[4], 1));
+      }
+    }
+
+    [Test]
+    public void TestTransformBlock_OutputBufferRangeNegative()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformBlock(new byte[3], 0, 3, new byte[4], -1));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock()
+    {
+      foreach (var pattern in new[] {
+        new {Input = "A",     Output = "QQ=="},
+        new {Input = "AS",    Output = "QVM="},
+        new {Input = "ASC",   Output = "QVND"},
+      }) {
+        using (var t = new ToBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+
+          var ret = t.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+
+          Assert.AreEqual(pattern.Output.Length, ret.Length);
+          Assert.AreEqual(pattern.Output, Encoding.ASCII.GetString(ret));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferEmpty()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.IsEmpty(t.TransformFinalBlock(new byte[0], 0, 0));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferTooLong()
+    {
+      foreach (var pattern in new[] {
+        new {Input = "ASCI",  Output = "QVNDSQ=="},
+        new {Input = "ASCII", Output = "QVNDSUk="},
+      }) {
+        using (var t = new ToBase64Transform()) {
+          var inputBuffer = Encoding.ASCII.GetBytes(pattern.Input);
+
+          Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length));
+        }
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferNull()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentNullException>(() => t.TransformFinalBlock(null, 0, 3));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferInvalidLength()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformFinalBlock(new byte[3], 0, 4));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferInvalidOffset()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformFinalBlock(new byte[3], 1, 3));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferRangeNegative()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentOutOfRangeException>(() => t.TransformFinalBlock(new byte[3], -1, 3));
+      }
+    }
+
+    [Test]
+    public void TestTransformFinalBlock_InputBufferRangeOverflow()
+    {
+      using (var t = new ToBase64Transform()) {
+        Assert.Throws<ArgumentException>(() => t.TransformFinalBlock(new byte[3], 1, int.MaxValue));
+      }
+    }
+  }
+}
