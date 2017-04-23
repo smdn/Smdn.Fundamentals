dc5022b69a9a2a3ecddf1e0e7286cac1f4be6a9f
smdn <smdn@smdn.jp>
2017-04-24 04:28:27 +0900

    added implementation of (From|To)Base64Transform

--- Smdn/src/Smdn.Security.Cryptography/FromBase64Transform.cs
+++ Smdn/src/Smdn.Security.Cryptography/FromBase64Transform.cs
@@ -27,37 +27,156 @@ using System.Security.Cryptography;
 
 namespace Smdn.Security.Cryptography {
   public class FromBase64Transform : ICryptoTransform {
-    public bool CanReuseTransform => throw new NotImplementedException();
-
-    public bool CanTransformMultipleBlocks => throw new NotImplementedException();
-
-    public int InputBlockSize => throw new NotImplementedException();
-
-    public int OutputBlockSize => throw new NotImplementedException();
+    public bool CanReuseTransform => true;
+    public bool CanTransformMultipleBlocks => false;
+    public int InputBlockSize => 1;
+    public int OutputBlockSize => 3;
 
     public FromBase64Transform()
+      : this(FromBase64TransformMode.IgnoreWhiteSpaces)
     {
-      throw new NotImplementedException();
     }
 
     public FromBase64Transform(FromBase64TransformMode mode)
     {
-      throw new NotImplementedException();
+      ignoreWhiteSpaces = (mode == FromBase64TransformMode.IgnoreWhiteSpaces);
     }
 
     public void Dispose()
     {
-      throw new NotImplementedException();
+      disposed = true;
     }
 
     public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
     {
-      throw new NotImplementedException();
+      if (disposed)
+        throw new ObjectDisposedException(GetType().FullName);
+
+      if (inputBuffer == null)
+        throw new ArgumentNullException("inputBuffer");
+      if (inputOffset < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputOffset", inputOffset);
+      if (inputCount < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputCount", inputCount);
+      if (inputBuffer.Length - inputCount < inputOffset)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("inputOffset", inputBuffer, inputOffset, inputCount);
+
+      if (outputBuffer == null)
+        throw new ArgumentNullException("outputBuffer");
+      if (outputOffset < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("outputOffset", outputOffset);
+      if (outputBuffer.Length <= outputOffset)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("outputOffset", outputBuffer, outputOffset, outputBuffer.Length);
+
+      var ret = 0;
+      var padded = false;
+      var decoded = new int[4];
+
+      for(;;) {
+        if (inputCount < 4)
+          break;
+
+        if (padded)
+          throw new FormatException("incorrect padding");
+
+        for (var b = 0; b < 4;) {
+          if (inputCount <= 0)
+            throw new FormatException("invalid format");
+
+          var octet = inputBuffer[inputOffset++];
+
+          inputCount--;
+
+          if (0x80 <= octet)
+            throw new FormatException($"invalid octet: 0x{octet:X2}");
+
+          decoded[b] = fromBase64Table[octet];
+
+          if (decoded[b] == -1) {
+            if (Char.IsWhiteSpace((char)octet)) {
+              if (ignoreWhiteSpaces)
+                continue;
+              else
+                throw new FormatException($"invalid octet: 0x{octet:X2}");
+            }
+            else {
+              throw new FormatException($"invalid octet: 0x{octet:X2}");
+            }
+          }
+
+          b++;
+        }
+
+        if (0x40 == decoded[0] || 0x40 == decoded[1])
+          throw new FormatException("incorrect padding");
+
+        if (outputBuffer.Length <= outputOffset)
+          throw new FormatException("attempt to access beyond end of array"); // NET46 spec
+
+        outputBuffer[outputOffset++] = (byte)((decoded[0] << 2) | (decoded[1] >> 4));
+        ret++;
+
+        if (0x40 == decoded[2]) {
+          padded = true;
+          continue;
+        }
+
+        if (outputBuffer.Length <= outputOffset)
+          throw new FormatException("attempt to access beyond end of array"); // NET46 spec
+
+        outputBuffer[outputOffset++] = (byte)((decoded[1] << 4) | (decoded[2] >> 2));
+        ret++;
+
+        if (0x40 == decoded[3]) {
+          padded = true;
+          continue;
+        }
+
+        if (outputBuffer.Length <= outputOffset)
+          throw new FormatException("attempt to access beyond end of array"); // NET46 spec
+
+        outputBuffer[outputOffset++] = (byte)((decoded[2] << 6) | decoded[3]);
+        ret++;
+      }
+
+      return ret;
     }
 
     public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
     {
-      throw new NotImplementedException();
+      if (disposed)
+        throw new ObjectDisposedException(GetType().FullName);
+
+      if (inputBuffer == null)
+        throw new ArgumentNullException("inputBuffer");
+      if (inputOffset < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputOffset", inputOffset);
+      if (inputCount < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputCount", inputCount);
+      if (inputBuffer.Length - inputCount < inputOffset)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("inputOffset", inputBuffer, inputOffset, inputCount);
+
+      var ret = new byte[(inputCount / 4 + 1) * 3];
+      var len = TransformBlock(inputBuffer, inputOffset, inputCount, ret, 0);
+
+      Array.Resize(ref ret, len);
+
+      return ret;
     }
+
+    private static readonly int[] fromBase64Table = new int[] {
+    /*0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,*/
+        -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, // 0x00
+        -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, // 0x10
+        -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, 0x3e,   -1,   -1,   -1, 0x3f, // 0x20
+        -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, 0x40,   -1,   -1, // 0x30
+        -1, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, // 0x40
+      0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19,   -1,   -1,   -1,   -1,   -1, // 0x50
+        -1, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, // 0x60
+      0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33,   -1,   -1,   -1,   -1,   -1, // 0x70
+    };
+
+    private bool disposed = false;
+    private readonly bool ignoreWhiteSpaces;
   }
 }
--- Smdn/src/Smdn.Security.Cryptography/ToBase64Transform.cs
+++ Smdn/src/Smdn.Security.Cryptography/ToBase64Transform.cs
@@ -27,32 +27,127 @@ using System.Security.Cryptography;
 
 namespace Smdn.Security.Cryptography {
   public class ToBase64Transform : ICryptoTransform {
-    public bool CanReuseTransform => throw new NotImplementedException();
-
-    public bool CanTransformMultipleBlocks => throw new NotImplementedException();
-
-    public int InputBlockSize => throw new NotImplementedException();
-
-    public int OutputBlockSize => throw new NotImplementedException();
+    public bool CanReuseTransform => true;
+    public bool CanTransformMultipleBlocks => false;
+    public int InputBlockSize => 3;
+    public int OutputBlockSize => 4;
 
     public ToBase64Transform()
     {
-      throw new NotImplementedException();
     }
 
     public void Dispose()
     {
-      throw new NotImplementedException();
     }
 
     public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
     {
-      throw new NotImplementedException();
+      if (inputBuffer == null)
+        throw new ArgumentNullException("inputBuffer");
+      if (inputOffset < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputOffset", inputOffset);
+      if (inputCount < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputCount", inputCount);
+      if (inputBuffer.Length - inputCount < inputOffset)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("inputOffset", inputBuffer, inputOffset, inputCount);
+      if (inputCount < InputBlockSize)
+        throw ExceptionUtils.CreateArgumentMustBeGreaterThan(InputBlockSize, "inputCount", inputCount);
+
+      if (outputBuffer == null)
+        throw new ArgumentNullException("outputBuffer");
+      if (outputOffset < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("outputOffset", outputOffset);
+      if (outputBuffer.Length <= outputOffset)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("outputOffset", outputBuffer, outputOffset, outputBuffer.Length);
+      if (outputBuffer.Length < outputOffset + OutputBlockSize)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("outputOffset + OutputBlockSize", outputBuffer, outputOffset, outputBuffer.Length);
+
+      return UncheckedTransformBlock(inputBuffer, inputOffset, inputCount, outputBuffer, outputOffset);
+    }
+
+    private static int UncheckedTransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+    {
+      const byte padding = 0x3d; // '='
+
+      var ret = 0;
+      int b = 0;
+
+      b = ((int)inputBuffer[inputOffset] & 0xfc) >> 2;
+
+      outputBuffer[outputOffset++] = toBase64Table[b];
+      ret++;
+
+      b = ((int)inputBuffer[inputOffset++] & 0x03 ) << 4;
+      inputCount--;
+
+      if (0 < inputCount) {
+        b |= ((int)inputBuffer[inputOffset] & 0xf0) >> 4;
+
+        outputBuffer[outputOffset++] = toBase64Table[b];
+        ret++;
+
+        b = ((int)inputBuffer[inputOffset++] & 0x0f) << 2;
+        inputCount--;
+
+        if (0 < inputCount) {
+          b |= ((int)inputBuffer[inputOffset] & 0xc0) >> 6;
+
+          outputBuffer[outputOffset++] = toBase64Table[b];
+          ret++;
+
+          b = ((int)inputBuffer[inputOffset++] & 0x3f);
+          inputCount--;
+
+          outputBuffer[outputOffset++] = toBase64Table[b];
+          ret++;
+        }
+        else {
+          outputBuffer[outputOffset++] = toBase64Table[b];
+          outputBuffer[outputOffset++] = padding;
+          ret += 2;
+        }
+      }
+      else {
+        outputBuffer[outputOffset++] = toBase64Table[b];
+        outputBuffer[outputOffset++] = padding;
+        outputBuffer[outputOffset++] = padding;
+        ret += 3;
+      }
+
+      return ret;
     }
 
     public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
     {
-      throw new NotImplementedException();
+      if (inputBuffer == null)
+        throw new ArgumentNullException("inputBuffer");
+      if (inputOffset < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputOffset", inputOffset);
+      if (inputCount < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive("inputCount", inputCount);
+      if (inputBuffer.Length - inputCount < inputOffset)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfArray("inputOffset", inputBuffer, inputOffset, inputCount);
+      if (InputBlockSize < inputCount)
+        throw ExceptionUtils.CreateArgumentMustBeLessThanOrEqualTo(InputBlockSize, "inputCount", inputCount);
+
+      if (inputCount == 0)
+        return Array.Empty<byte>();
+
+      var ret = new byte[OutputBlockSize];
+      var len = UncheckedTransformBlock(inputBuffer, inputOffset, inputCount, ret, 0);
+
+      Array.Resize(ref ret, len);
+
+      return ret;
     }
+
+    private static readonly byte[] toBase64Table = new byte[] {
+    /*0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,*/
+      0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, // 0x00
+      0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, // 0x10
+      0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, // 0x20
+      0x77, 0x78, 0x79, 0x7a, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x2b, 0x2f, // 0x30
+    /*0x3d,*/
+    };
   }
 }
