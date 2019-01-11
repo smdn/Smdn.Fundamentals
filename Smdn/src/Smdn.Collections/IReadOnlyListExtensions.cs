04dba2d1ccb24c0808d10b6c1c061d9e7fa1760a
smdn <smdn@smdn.jp>
2019-01-11 13:06:58 +0900

    add IReadOnlyListExtensions.Slice

---
+++ Smdn/src/Smdn.Collections/IReadOnlyListExtensions.cs
@@ -0,0 +1,96 @@
+// 
+// Copyright (c) 2019 smdn <smdn@smdn.jp>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Smdn.Collections {
+  public static class IReadOnlyListExtensions {
+    public static IReadOnlyList<T> Slice<T>(this IReadOnlyList<T> list, int index)
+    {
+      if (list == null)
+        throw new ArgumentNullException(nameof(list));
+
+      return Slice(list, index, list.Count - index);
+    }
+
+    public static IReadOnlyList<T> Slice<T>(this IReadOnlyList<T> list, int index, int count)
+    {
+      if (list == null)
+        throw new ArgumentNullException(nameof(list));
+      if (index < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive(nameof(index), index);
+      if (count < 0)
+        throw ExceptionUtils.CreateArgumentMustBeZeroOrPositive(nameof(count), count);
+      if (list.Count - count < index)
+        throw ExceptionUtils.CreateArgumentAttemptToAccessBeyondEndOfCollection(nameof(index), list, index, count);
+
+      if (count == 0)
+#if NET45 || NET452
+        return ArrayExtensions.Empty<T>();
+#else
+        return Array.Empty<T>();
+#endif
+
+      if (list is T[] arr)
+        return new ArraySegment<T>(arr, index, count);
+      if (list is ArraySegment<T> seg)
+        return new ArraySegment<T>(seg.Array, seg.Offset + index, count);
+        // #if .NET Standard 2.1, .NET Core 2.0
+        // return seg.Slice(start, count);
+
+      return new ReadOnlyListSegment<T>(list, index, count);
+    }
+
+    private class ReadOnlyListSegment<T> : IReadOnlyList<T> {
+      private readonly IReadOnlyList<T> list;
+      private readonly int offset;
+      public int Count { get; }
+
+      public T this[int index] {
+        get {
+          if (index < 0 || Count <= index)
+            throw ExceptionUtils.CreateArgumentMustBeInRange(0, nameof(Count), nameof(index), index);
+
+          return list[offset + index];
+        }
+      }
+
+      public ReadOnlyListSegment(IReadOnlyList<T> list, int offset, int count)
+      {
+        this.list = list;
+        this.offset = offset;
+        Count = count;
+      }
+
+      public IEnumerator<T> GetEnumerator()
+      {
+        // XXX
+        foreach (var e in list.Skip(offset).Take(Count))
+          yield return e;
+      }
+
+      IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+  }
+}
---
+++ Smdn/test/Smdn.Collections/IReadOnlyListExtensions.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Smdn.Collections {
+  [TestFixture]
+  public class IReadOnlyListExtensionsTests {
+    [Test]
+    public void TestSlice_Array()
+    {
+      TestSlice(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+    }
+
+    [Test]
+    public void TestSlice_ArraySegment()
+    {
+      TestSlice(new ArraySegment<int>(new[] { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1, 10));
+    }
+
+    [Test]
+    public void TestSlice_List()
+    {
+      TestSlice(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+    }
+
+    private void TestSlice(IReadOnlyList<int> list)
+    {
+      Assert.AreEqual(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, list.Slice(0));
+      Assert.AreEqual(new[] { 0, 1, 2 }, list.Slice(0, 3));
+      Assert.AreEqual(new[] { 7, 8, 9 }, list.Slice(7, 3));
+      Assert.AreEqual(new[] { 2, 3, 4, 5, 6 }, list.Slice(2, 5));
+      Assert.AreEqual(new[] { 6, 7, 8, 9 }, list.Slice(6));
+      Assert.AreEqual(new int[] { }, list.Slice(0, 0));
+      Assert.AreEqual(new int[] { }, list.Slice(10));
+      Assert.AreEqual(new int[] { }, list.Slice(10, 0));
+
+      Assert.Throws<ArgumentOutOfRangeException>(() => list.Slice(-1, 1), "argument exception #1");
+      Assert.Throws<ArgumentOutOfRangeException>(() => list.Slice(1, -1), "argument exception #2");
+      Assert.Throws<ArgumentException>(() => list.Slice(10, 1), "argument exception #3");
+      Assert.Throws<ArgumentException>(() => list.Slice(1, 10), "argument exception #4");
+      Assert.Throws<ArgumentException>(() => list.Slice(11, 0), "argument exception #5");
+      Assert.Throws<ArgumentException>(() => list.Slice(0, 11), "argument exception #6");
+
+      // test IReadOnlyList.Count
+      Assert.AreEqual(10, list.Slice(0).Count);
+      Assert.AreEqual(3, list.Slice(0, 3).Count);
+      Assert.AreEqual(4, list.Slice(6).Count);
+      Assert.AreEqual(0, list.Slice(10).Count);
+
+      // test IReadOnlyList.this[]
+      Assert.AreEqual(2, list.Slice(2, 3)[0]);
+      Assert.AreEqual(3, list.Slice(2, 3)[1]);
+      Assert.AreEqual(4, list.Slice(2, 3)[2]);
+      Assert.Throws<ArgumentOutOfRangeException>(() => Assert.IsNotNull(list.Slice(2, 3)[-1]));
+      Assert.Throws<ArgumentOutOfRangeException>(() => Assert.IsNotNull(list.Slice(2, 3)[3]));
+
+      Assert.Throws<ArgumentOutOfRangeException>(() => Assert.IsNotNull(list.Slice(0, 0)[0]));
+
+      // test enumerator
+      Assert.AreEqual(new[] { 0, 1, 2 }, list.Slice(0, 3).ToArray());
+      Assert.AreEqual(new[] { 7, 8, 9 }, list.Slice(7, 3).ToArray());
+      Assert.AreEqual(new int[] { }, list.Slice(0, 0).ToArray());
+      Assert.AreEqual(new int[] { }, list.Slice(10).ToArray());
+    }
+  }
+}
