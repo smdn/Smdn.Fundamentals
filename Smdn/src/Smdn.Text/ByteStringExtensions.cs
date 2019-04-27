2855c4e1b2b2b287114d60170748b7b91cb0d206
smdn <smdn@smdn.jp>
2019-04-27 20:50:26 +0900

    add ImapResponseReceiver.ReceiveResponseAsync() and reimplement with ReadOnlySequence<byte> instead of ByteString

--- Smdn/src/Smdn.Text/ByteStringExtensions.cs
+++ Smdn/src/Smdn.Text/ByteStringExtensions.cs
@@ -29,6 +29,12 @@ namespace Smdn.Text {
       return new ReadOnlySequence<byte>(str.Segment.AsMemory());
     }
 
+    [Obsolete()]
+    public static ByteString ToByteString(this ReadOnlySequence<byte> sequence)
+    {
+      return ByteString.CreateImmutable(sequence.ToArray());
+    }
+
     public static bool SequenceEqual(this ReadOnlySequence<byte> sequence, ReadOnlySpan<byte> value)
     {
       if (sequence.Length != value.Length)
@@ -58,6 +64,38 @@ namespace Smdn.Text {
         return false;
 
       return SequenceEqual(sequence.Slice(0, value.Length), value);
+    }
+
+    public static unsafe byte[] ToArrayUpperCase(this ReadOnlySequence<byte> sequence)
+    {
+      if (sequence.IsEmpty)
+#if NET45 || NET452
+        return ArrayExtensions.Empty<byte>();
+#else
+        return Array.Empty<byte>();
+#endif
+
+      var bytes = new byte[sequence.Length];
+
+      fixed (byte* b0 = bytes) {
+        byte* b = b0;
+
+        var position = sequence.Start;
+
+        while (sequence.TryGet(ref position, out var memory, advance: true)) {
+          var span = memory.Span;
+
+          fixed (byte* s0 = span) {
+            byte* s = s0;
+
+            for (var i = 0; i < span.Length; i++) {
+              *b++ = Ascii.Octets.ToUpperCaseAsciiTableArray[*s++];
+            }
+          }
+        }
+      }
+
+      return bytes;
     }
   }
 }
