78635701aa1aed3d29bc94713f13e7833933adfa
smdn <smdn@smdn.jp>
2020-09-17 04:12:00 +0900

    add overload that can specify the start index of input stirng

--- Smdn/src/Smdn.Text.RegularExpressions/RegexExtensions.cs
+++ Smdn/src/Smdn.Text.RegularExpressions/RegexExtensions.cs
@@ -25,11 +25,14 @@ using System.Text.RegularExpressions;
 namespace Smdn.Text.RegularExpressions {
   public static class RegexExtensions {
     public static bool IsMatch(this Regex regex, string input, out Match match)
+      => IsMatch(regex, input, startIndex: 0, out match);
+
+    public static bool IsMatch(this Regex regex, string input, int startIndex, out Match match)
     {
       if (regex == null)
         throw new ArgumentNullException(nameof(regex));
 
-      match = regex.Match(input);
+      match = regex.Match(input, startIndex);
 
       return match.Success;
     }
