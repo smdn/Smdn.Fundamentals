03f7db500d6756e7f917b9d250d3f9fd56e8ac2f
smdn <smdn@smdn.jp>
2021-08-05 21:50:05 +0900

    split off Smdn.Fundamentals.Collections

--- src/Smdn/AssemblyAttributes.cs
+++ src/Smdn/AssemblyAttributes.cs
@@ -6,6 +6,17 @@ using System.Runtime.CompilerServices;
 [assembly: System.CLSCompliant(true)]
 
 /*
+ * Smdn.Fundamentals.Collections
+ */
+[assembly: TypeForwardedTo(typeof(Smdn.Collections.IReadOnlyCollectionExtensions))]
+[assembly: TypeForwardedTo(typeof(Smdn.Collections.IReadOnlyListExtensions))]
+#if !(NETSTANDARD2_1)
+[assembly: TypeForwardedTo(typeof(Smdn.Collections.KeyValuePair))]
+#endif
+[assembly: TypeForwardedTo(typeof(Smdn.Collections.ReadOnlyDictionary<,>))]
+[assembly: TypeForwardedTo(typeof(Smdn.Collections.Singleton))]
+
+/*
  * Smdn.Fundamentals.Encodings
  */
 [assembly: TypeForwardedTo(typeof(Smdn.Text.Encodings.EncodingNotSupportedException))]
