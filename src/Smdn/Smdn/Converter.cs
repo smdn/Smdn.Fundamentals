916b71b9d1b349dc83f16cfd3d5561277c23f5f6
smdn <smdn@smdn.jp>
2021-08-08 19:24:05 +0900

    fix target framework switch

--- src/Smdn/Smdn/Converter.cs
+++ src/Smdn/Smdn/Converter.cs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2017 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 namespace Smdn {
-#if !(NETFRAMEWORK || NETSTANDARD2_0 || NETSTANDARD2_1)
+#if !(NETFRAMEWORK || NETSTANDARD2_0_OR_GREATER || NET5_0_OR_GREATER)
   public delegate TOutput Converter<in TInput, out TOutput>(TInput input);
 #endif
 }
