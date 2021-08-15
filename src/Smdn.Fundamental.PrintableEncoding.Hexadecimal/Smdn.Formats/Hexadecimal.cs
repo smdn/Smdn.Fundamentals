297c78662e724640d0a9f6984163269da4cedb0a
smdn <smdn@smdn.jp>
2021-08-16 06:12:02 +0900

    add overload of Hexadecimal.TryDecode which accepts char sequence

--- src/Smdn.Fundamental.PrintableEncoding.Hexadecimal/Smdn.Formats/Hexadecimal.cs
+++ src/Smdn.Fundamental.PrintableEncoding.Hexadecimal/Smdn.Formats/Hexadecimal.cs
@@ -57,6 +57,46 @@ namespace Smdn.Formats {
       return true;
     }
 
+    public static bool TryDecodeUpperCase(ReadOnlySpan<char> upperCaseDataSequence, Span<byte> destination, out int decodedLength)
+      => TryDecode(upperCaseDataSequence, destination, allowUpperCase: true, allowLowerCase: false, out decodedLength);
+
+    public static bool TryDecodeLowerCase(ReadOnlySpan<char> lowerCaseDataSequence, Span<byte> destination, out int decodedLength)
+      => TryDecode(lowerCaseDataSequence, destination, allowUpperCase: false, allowLowerCase: true, out decodedLength);
+
+    public static bool TryDecode(ReadOnlySpan<char> dataSequence, Span<byte> destination, out int decodedLength)
+      => TryDecode(dataSequence, destination, allowUpperCase: true, allowLowerCase: true, out decodedLength);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static bool TryDecode(ReadOnlySpan<char> dataSequence, Span<byte> destination, bool allowUpperCase, bool allowLowerCase, out int decodedLength)
+    {
+      decodedLength = 0;
+
+      if (dataSequence.IsEmpty)
+        return true;
+
+      var length = dataSequence.Length;
+
+      if ((length & 0x1) != 0)
+        return false; // incorrect form
+
+      length >>= 1;
+
+      if (destination.Length < length)
+        return false; // destination too short
+
+      for (var i = 0; i < length; i++) {
+        if (!TryDecode(dataSequence.Slice(0, 2), allowUpperCase, allowLowerCase, out byte decodedData))
+          return false;
+
+        destination[i] = decodedData;
+
+        dataSequence = dataSequence.Slice(2);
+        decodedLength++;
+      }
+
+      return true;
+    }
+
     public static bool TryDecodeUpperCase(ReadOnlySpan<byte> upperCaseData, out byte decodedData)
       => TryDecode(upperCaseData, allowUpperCase: true, allowLowerCase: false, out decodedData);
 
