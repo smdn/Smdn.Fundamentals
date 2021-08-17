c33007c3c42723340d96d510250250e590752e0d
smdn <smdn@smdn.jp>
2021-08-18 05:10:55 +0900

    make Smdn.Formats.Html.* obsolete

--- src/Smdn/Smdn.Formats/Html.cs
+++ src/Smdn/Smdn.Formats/Html.cs
@@ -4,130 +4,48 @@ using System;
 using System.Text;
 using System.Text.RegularExpressions;
 
-using Smdn.Text;
+using Smdn.Xml.Xhtml;
 
 namespace Smdn.Formats {
   public static class Html {
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.EscapeHtml instead")]
     public static string ToHtmlEscapedString(string str)
-    {
-      if (str == null)
-        throw new ArgumentNullException(nameof(str));
+      => HtmlConvert.EscapeHtml((str ?? throw new ArgumentNullException(nameof(str))).AsSpan());
 
-      return ToXhtmlEscapedString(str, false);
-    }
-
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.EscapeXhtml instead")]
     public static string ToXhtmlEscapedString(string str)
-    {
-      if (str == null)
-        throw new ArgumentNullException(nameof(str));
+      => HtmlConvert.EscapeXhtml((str ?? throw new ArgumentNullException(nameof(str))).AsSpan());
 
-      return ToXhtmlEscapedString(str, true);
-    }
-
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.EscapeHtml instead")]
     public static string ToHtmlEscapedStringNullable(string str)
-    {
-      if (str == null)
-        return null;
-      else
-        return ToXhtmlEscapedString(str, false);
-    }
+      => str is null ? null : HtmlConvert.EscapeHtml(str.AsSpan());
 
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.EscapeXhtml instead")]
     public static string ToXhtmlEscapedStringNullable(string str)
-    {
-      if (str == null)
-        return null;
-      else
-        return ToXhtmlEscapedString(str, true);
-    }
+      => str is null ? null : HtmlConvert.EscapeXhtml(str.AsSpan());
 
-    private static string ToXhtmlEscapedString(string str, bool xhtml)
-    {
-      var sb = new StringBuilder(str.Length);
-      var len = str.Length;
-
-      for (var i = 0; i < len; i++) {
-        var ch = str[i];
-
-        switch (ch) {
-          case Ascii.Chars.Ampersand:   sb.Append("&amp;"); break;
-          case Ascii.Chars.LessThan:    sb.Append("&lt;"); break;
-          case Ascii.Chars.GreaterThan: sb.Append("&gt;"); break;
-          case Ascii.Chars.DQuote:      sb.Append("&quot;"); break;
-          case Ascii.Chars.Quote:
-            if (xhtml) sb.Append("&apos;");
-            else sb.Append(Ascii.Chars.Quote);
-            break;
-          default: sb.Append(ch); break;
-        }
-      }
-
-      return sb.ToString();
-    }
-
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.UnescapeHtml instead")]
     public static string FromHtmlEscapedString(string str)
-    {
-      return FromXhtmlEscapedString(str, false);
-    }
+      => HtmlConvert.UnescapeHtml((str ?? throw new ArgumentNullException(nameof(str))).AsSpan());
 
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.UnescapeHtml instead")]
     public static string FromHtmlEscapedStringNullable(string str)
-    {
-      if (str == null)
-        return null;
-      else
-        return FromXhtmlEscapedString(str, false);
-    }
+      => str is null ? null : HtmlConvert.UnescapeHtml(str.AsSpan());
 
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.UnescapeXhtml instead")]
     public static string FromXhtmlEscapedString(string str)
-    {
-      return FromXhtmlEscapedString(str, true);
-    }
+      => HtmlConvert.UnescapeXhtml((str ?? throw new ArgumentNullException(nameof(str))).AsSpan());
 
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.UnescapeXhtml instead")]
     public static string FromXhtmlEscapedStringNullable(string str)
-    {
-      if (str == null)
-        return null;
-      else
-        return FromXhtmlEscapedString(str, true);
-    }
+      => str is null ? null : HtmlConvert.UnescapeXhtml(str.AsSpan());
 
-    private static string FromXhtmlEscapedString(string str, bool xhtml)
-    {
-      if (str == null)
-        throw new ArgumentNullException(nameof(str));
-
-      var sb = new StringBuilder(str);
-
-      sb.Replace("&lt;", "<");
-      sb.Replace("&gt;", ">");
-      sb.Replace("&quot;", "\"");
-
-      if (xhtml)
-        sb.Replace("&apos;", "'");
-
-      sb.Replace("&amp;", "&");
-
-      return sb.ToString();
-    }
-
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.DecodeNumericCharacterReference instead")]
     public static string FromNumericCharacterReferenceNullable(string str)
-    {
-      if (str == null)
-        return null;
-      else
-        return FromNumericCharacterReference(str);
-    }
+      => str is null ? null : HtmlConvert.DecodeNumericCharacterReference(str);
 
+    [Obsolete("use Smdn.Xml.Xhtml.HtmlConvert.DecodeNumericCharacterReference instead")]
     public static string FromNumericCharacterReference(string str)
-    {
-      if (str == null)
-        throw new ArgumentNullException(nameof(str));
-
-      return Regex.Replace(str, @"&#(?<hex>x?)(?<number>[0-9a-fA-F]+);", delegate(Match m) {
-        if (m.Groups["hex"].Length == 0)
-          return ((char)Convert.ToUInt16(m.Groups["number"].Value, 10)).ToString();
-        else
-          return ((char)Convert.ToUInt16(m.Groups["number"].Value, 16)).ToString();
-      });
-    }
+      => HtmlConvert.DecodeNumericCharacterReference(str ?? throw new ArgumentNullException(nameof(str)));
   }
 }
