aecb6ea3889451413700356a08643cb1d4eb3459
smdn <smdn@smdn.jp>
2021-08-18 05:49:50 +0900

    add UriExtensions

---
+++ src/Smdn.Fundamental.Uri/Smdn/UriExtensions.cs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using System.Collections.Generic;
+
+namespace Smdn {
+  public static class UriExtensions {
+    public static IReadOnlyDictionary<string, string> GetSplittedQueries(this Uri uri)
+      => UriUtils.SplitQueryParameters((uri ?? throw new ArgumentNullException(nameof(uri))).Query);
+  }
+}
