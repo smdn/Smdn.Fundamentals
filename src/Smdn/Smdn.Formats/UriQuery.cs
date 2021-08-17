5f4f88e1a4a44ddfcd4cafa0ba1aa613a2d90390
smdn <smdn@smdn.jp>
2021-08-18 05:52:09 +0900

    mark Smdn.Formats.UriQuery obsolete

--- src/Smdn/Smdn.Formats/UriQuery.cs
+++ src/Smdn/Smdn.Formats/UriQuery.cs
@@ -6,65 +6,49 @@ using System.Text;
 
 namespace Smdn.Formats {
   public static class UriQuery {
-    private const char parameterSplitterChar = '&';
-    private const char nameValueSplitterChar = '=';
-
+    [Obsolete("use Smdn.UriUtils.JoinQueryParameters instead")]
     public static string JoinQueryParameters(IEnumerable<KeyValuePair<string, string>> queryParameters)
-    {
-      if (queryParameters == null)
-        throw new ArgumentNullException(nameof(queryParameters));
+      => UriUtils.JoinQueryParameters(queryParameters ?? throw new ArgumentNullException(nameof(queryParameters)));
 
-      var sb = new StringBuilder();
-
-      foreach (var pair in queryParameters) {
-        if (0 < sb.Length) // one or more parameters
-          sb.Append(parameterSplitterChar);
-
-        sb.Append(pair.Key);
-
-        if (pair.Value != null) {
-          sb.Append(nameValueSplitterChar);
-          sb.Append(pair.Value);
-        }
-      }
-
-      return sb.ToString();
-    }
-
+    [Obsolete("use Smdn.UriUtils.SplitQueryParameters instead")]
     public static IDictionary<string, string> SplitQueryParameters(string queryParameters)
     {
-      return SplitQueryParameters(queryParameters, EqualityComparer<string>.Default);
+      var ret = UriUtils.SplitQueryParameters(queryParameters ?? throw new ArgumentNullException(nameof(queryParameters)));
+
+      return ret is IDictionary<string, string> dic
+        ? dic
+        : ToDictionary(ret, EqualityComparer<string>.Default);
     }
 
+    [Obsolete("use Smdn.UriUtils.SplitQueryParameters instead")]
     public static IDictionary<string, string> SplitQueryParameters(string queryParameters, IEqualityComparer<string> comparer)
     {
-      if (queryParameters == null)
-        throw new ArgumentNullException(nameof(queryParameters));
-      if (comparer == null)
-        throw new ArgumentNullException(nameof(comparer));
+      var ret = UriUtils.SplitQueryParameters(
+        queryParameters ?? throw new ArgumentNullException(nameof(queryParameters)),
+        comparer
+      );
 
-      var ret = new Dictionary<string, string>(comparer);
+      return ret is IDictionary<string, string> dic
+        ? dic
+        : ToDictionary(ret, comparer);
+    }
 
-      if (queryParameters.StartsWith('?'))
-        queryParameters = queryParameters.Substring(1);
+    private static IDictionary<string, String> ToDictionary(
+      IReadOnlyDictionary<string, string> dic,
+      IEqualityComparer<string> comparer
+    )
+    {
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+      return new Dictionary<string, string>(dic, comparer);
+#else
+      var ret = new Dictionary<string, string>(dic.Count, comparer);
 
-      if (queryParameters.Length == 0)
-        return ret;
-
-      var splitted = queryParameters.Split(new[] {parameterSplitterChar}, StringSplitOptions.RemoveEmptyEntries);
-
-      foreach (var nameAndValue in splitted) {
-        var pos = nameAndValue.IndexOf(nameValueSplitterChar);
-
-        if (pos < 0)
-          // name only
-          ret[nameAndValue] = string.Empty;
-        else
-          // name = value
-          ret[nameAndValue.Substring(0, pos)] = nameAndValue.Substring(pos + 1);
+      foreach (var pair in dic) {
+        ret[pair.Key] = pair.Value;
       }
 
       return ret;
+#endif
     }
   }
 }
