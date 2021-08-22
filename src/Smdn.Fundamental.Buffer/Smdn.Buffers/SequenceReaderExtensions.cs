cce7d91936c7a6e4e4639a9fee7bcf5ea41d4d65
smdn <smdn@smdn.jp>
2021-08-23 05:56:53 +0900

    apply MethodImplOptions.AggressiveInlining

--- src/Smdn.Fundamental.Buffer/Smdn.Buffers/SequenceReaderExtensions.cs
+++ src/Smdn.Fundamental.Buffer/Smdn.Buffers/SequenceReaderExtensions.cs
@@ -3,10 +3,12 @@
 
 using System;
 using System.Buffers;
+using System.Runtime.CompilerServices;
 
 namespace Smdn.Buffers {
 #if NETSTANDARD2_1_OR_GREATER
   public static class SequenceReaderExtensions {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ReadOnlySequence<T> GetUnreadSequence<T>(this SequenceReader<T> sequenceReader) where T : unmanaged, IEquatable<T>
 #if NET5_0_OR_GREATER
       => sequenceReader.UnreadSequence;
