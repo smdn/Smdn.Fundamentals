7d8000efc65bbd201ccbed7f9acdded234340c26
smdn <smdn@smdn.jp>
2021-08-23 05:56:21 +0900

    enable extensions on net5.0

--- src/Smdn.Fundamental.Buffer/Smdn.Buffers/SequenceReaderExtensions.cs
+++ src/Smdn.Fundamental.Buffer/Smdn.Buffers/SequenceReaderExtensions.cs
@@ -5,10 +5,14 @@ using System;
 using System.Buffers;
 
 namespace Smdn.Buffers {
-#if NETSTANDARD2_1_OR_GREATER && !NET5_0_OR_GREATER
+#if NETSTANDARD2_1_OR_GREATER
   public static class SequenceReaderExtensions {
     public static ReadOnlySequence<T> GetUnreadSequence<T>(this SequenceReader<T> sequenceReader) where T : unmanaged, IEquatable<T>
+#if NET5_0_OR_GREATER
+      => sequenceReader.UnreadSequence;
+#else
       => sequenceReader.Sequence.Slice(sequenceReader.Position);
+#endif
   }
 #endif
 }
