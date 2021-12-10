82aa695a0342ac68a74626c7dbeac1c8ef38e69c
smdn <smdn@smdn.jp>
2021-12-10 19:02:41 +0900

    follow the code analyzer rules

--- src/Smdn.Fundamental.String.Replacement/Smdn/ReplaceCharEvaluator.cs
+++ src/Smdn.Fundamental.String.Replacement/Smdn/ReplaceCharEvaluator.cs
@@ -1,7 +1,5 @@
 // SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
-using System;
-using System.Text;
 
 namespace Smdn {
   public delegate string ReplaceCharEvaluator(char ch, string str, int index);
