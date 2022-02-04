574fb8d6631ca78beee2cddf2078a3d5ee44e8f6
smdn <smdn@smdn.jp>
2022-02-05 03:25:21 +0900

    fix indent

--- src/Smdn.Fundamental.Shell/Smdn.OperatingSystem/ShellString.cs
+++ src/Smdn.Fundamental.Shell/Smdn.OperatingSystem/ShellString.cs
@@ -100,7 +100,7 @@ public class ShellString :
   {
     if (Raw == null)
 #pragma warning disable CA2201
-        // TODO: throw InvalidOperationException or etc instead
+      // TODO: throw InvalidOperationException or etc instead
       throw new NullReferenceException();
 #pragma warning restore CA2201
     else
