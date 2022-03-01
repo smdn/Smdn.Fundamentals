f6e47c513a7a2c8ae1f94d3ec493a1abf0785a57
smdn <smdn@smdn.jp>
2022-03-01 23:44:33 +0900

    add test fixture for XEntityReference

---
+++ tests/Smdn.Fundamental.Xml.Linq/Smdn.Xml.Linq/XEntityReference.cs
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+
+namespace Smdn.Xml.Linq;
+
+[TestFixture()]
+public class XEntityReferenceTests {
+  [Test]
+  public void TestWriteTo()
+  {
+    var er = new XEntityReference("copy");
+    var sb = new StringBuilder();
+    var settings = new XmlWriterSettings() {
+      ConformanceLevel = ConformanceLevel.Fragment
+    };
+
+    using (var writer = XmlWriter.Create(new StringWriter(sb), settings)) {
+      er.WriteTo(writer);
+    }
+
+    Assert.AreEqual("&copy;", sb.ToString());
+  }
+}
