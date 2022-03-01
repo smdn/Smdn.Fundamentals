c213e69e5dec8277cfb8159bc81b7573893a590b
smdn <smdn@smdn.jp>
2022-03-01 22:05:35 +0900

    fix to test ArrayShim.ConvertAll

--- tests/Smdn.Fundamental.Shim/Smdn/ArrayShim.cs
+++ tests/Smdn.Fundamental.Shim/Smdn/ArrayShim.cs
@@ -5,36 +5,46 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
+#if SYSTEM_ARRAY_CONVERTALL
+using _Array = System.Array; // System.Array.ConvertAll
+#else
+using _Array = Smdn.ArrayShim; // Smdn.ArrayShim.ConvertAll
+#endif
+
 namespace Smdn;
 
 [TestFixture()]
 public class ArrayShimTests {
   [Test]
-  public void TestConvert()
+  public void TestConvertAll()
   {
     var array = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     var expected = new[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-    CollectionAssert.AreEqual(expected,
-                              array.Convert(i => i.ToString("D")));
+    CollectionAssert.AreEqual(
+      expected,
+      _Array.ConvertAll(array, i => i.ToString("D"))
+    );
   }
 
   [Test]
-  public void TestConvert_ArgumentArrayEmpty()
+  public void TestConvertAll_ArgumentArrayEmpty()
   {
-    Assert.IsEmpty(Enumerable.Empty<int>().ToArray().Convert(i => i));
+    Assert.IsEmpty(
+      _Array.ConvertAll(Enumerable.Empty<int>().ToArray(), i => i)
+    );
   }
 
   [Test]
-  public void TestConvert_ArgumentArrayNull()
+  public void TestConvertAll_ArgumentArrayNull()
   {
     int[] array = null;
 
-    Assert.Throws<ArgumentNullException>(() => array.Convert(i => i));
+    Assert.Throws<ArgumentNullException>(() => _Array.ConvertAll(array, i => i));
   }
 
   [Test]
-  public void TestConvert_ArgumentConverterNull()
+  public void TestConvertAll_ArgumentConverterNull()
   {
     var array = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 #if SYSTEM_CONVERTER
@@ -43,6 +53,6 @@ public class ArrayShimTests {
     Func<int, long> converter = null;
 #endif
 
-    Assert.Throws<ArgumentNullException>(() => array.Convert(converter));
+    Assert.Throws<ArgumentNullException>(() => _Array.ConvertAll(array, converter));
   }
 }
