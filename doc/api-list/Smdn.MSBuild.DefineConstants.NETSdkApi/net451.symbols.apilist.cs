543b513c1ddba682b338fbd73cd4df67c68ff9b8
smdn <smdn@smdn.jp>
2022-03-27 03:38:14 +0900

    update

--- doc/api-list/Smdn.MSBuild.DefineConstants.NETSdkApi/net451.symbols.apilist.cs
+++ doc/api-list/Smdn.MSBuild.DefineConstants.NETSdkApi/net451.symbols.apilist.cs
@@ -3,15 +3,15 @@
 //   InformationalVersion: 1.1.1
 
 // List of symbols defined on target framework 'net451'
-#define SYSTEM_ARRAY_CONVERTALL
-#define SYSTEM_CONVERTER
-#define SYSTEM_DIAGNOSTICS_PROCESS
-#define SYSTEM_EXCEPTION_CTOR_SERIALIZATIONINFO
-#define SYSTEM_ICLONEABLE
-#define SYSTEM_IO_STREAM_CLOSE
-#define SYSTEM_MATH_DIVREM
-#define SYSTEM_NET_NETWORKINFORMATION_PHYSICALADDRESS
-#define SYSTEM_RUNTIME_SERIALIZATION_FORMATTER_BINARY
-#define SYSTEM_RUNTIME_SERIALIZATION_SERIALIZATIONBINDER
-#define SYSTEM_SECURITY_CRYPTOGRAPHY_HASHALGORITHM_CLEAR
-#define SYSTEM_TEXT_ENCODING_DEFAULT_ANSI
+#define SYSTEM_ARRAY_CONVERTALL // System.Array.ConvertAll
+#define SYSTEM_CONVERTER // System.Converter
+#define SYSTEM_EXCEPTION_CTOR_SERIALIZATIONINFO // System.Exception.ctor(SerializationInfo)
+#define SYSTEM_ICLONEABLE // System.ICloneable
+#define SYSTEM_MATH_DIVREM // System.Math.DivRem
+#define SYSTEM_DIAGNOSTICS_PROCESS // System.Diagnostics.Process
+#define SYSTEM_IO_STREAM_CLOSE // System.IO.Stream.Close
+#define SYSTEM_NET_NETWORKINFORMATION_PHYSICALADDRESS // System.Net.NetworkInformation.PhysicalAddress
+#define SYSTEM_RUNTIME_SERIALIZATION_SERIALIZATIONBINDER // System.Runtime.Serialization.SerializationBinder
+#define SYSTEM_RUNTIME_SERIALIZATION_FORMATTER_BINARY // System.Runtime.Serialization.Formatter.Binary
+#define SYSTEM_SECURITY_CRYPTOGRAPHY_HASHALGORITHM_CLEAR // System.Security.Cryptography.HashAlgorithm.Clear
+#define SYSTEM_TEXT_ENCODING_DEFAULT_ANSI // System.Text.Encoding.Default_ANSI
--- doc/api-list/Smdn.MSBuild.DefineConstants.NETSdkApi/netstandard1.4.symbols.apilist.cs
+++ doc/api-list/Smdn.MSBuild.DefineConstants.NETSdkApi/netstandard1.4.symbols.apilist.cs
@@ -3,7 +3,7 @@
 //   InformationalVersion: 1.1.1
 
 // List of symbols defined on target framework 'netstandard1.4'
-#define SYSTEM_ARRAY_EMPTY
-#define SYSTEM_FORMATTABLESTRING
-#define SYSTEM_THREADING_TASKS_TASK_COMPLETEDTASK
-#define SYSTEM_THREADING_TASKS_TASK_FROMCANCELED
+#define SYSTEM_ARRAY_EMPTY // System.Array.Empty
+#define SYSTEM_FORMATTABLESTRING // System.FormattableString
+#define SYSTEM_THREADING_TASKS_TASK_COMPLETEDTASK // System.Threading.Tasks.Task.CompletedTask
+#define SYSTEM_THREADING_TASKS_TASK_FROMCANCELED // System.Threading.Tasks.Task.FromCanceled
