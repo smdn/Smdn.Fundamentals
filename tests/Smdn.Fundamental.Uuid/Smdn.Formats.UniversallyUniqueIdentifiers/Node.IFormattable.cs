9eb590191089f51c77d986a4dbf6eadaf1d76911
smdn <smdn@smdn.jp>
2022-04-01 14:18:10 +0900

    split files

---
+++ tests/Smdn.Fundamental.Uuid/Smdn.Formats.UniversallyUniqueIdentifiers/Node.IFormattable.cs
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Smdn.Formats.UniversallyUniqueIdentifiers;
+
+partial class NodeTests {
+  [Test]
+  public void TestToString()
+  {
+    var regexFormat_X = new Regex("^[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}$");
+    var regexFormat_x = new Regex("^[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}$");
+    var regexFormat_Default = regexFormat_X;
+
+    for (var n = 0; n < 1000; n++) {
+      var node = Node.CreateRandom();
+
+      Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString()), node.ToString());
+      Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString(null)), node.ToString(null));
+      Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString(string.Empty)), node.ToString(string.Empty));
+      Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString(null, formatProvider: null)), node.ToString(null, formatProvider: null));
+      Assert.IsTrue(regexFormat_X.IsMatch(node.ToString("X")), node.ToString("X"));
+      Assert.IsTrue(regexFormat_x.IsMatch(node.ToString("x")), node.ToString("x"));
+    }
+  }
+
+  [Test]
+  public void TestToString_InvalidFormat()
+  {
+    var node = Node.CreateRandom();
+
+    Assert.Throws<FormatException>(() => node.ToString("n"));
+    Assert.Throws<FormatException>(() => node.ToString("xx"));
+    Assert.Throws<FormatException>(() => node.ToString("XX"));
+  }
+}
--- tests/Smdn.Fundamental.Uuid/Smdn.Formats.UniversallyUniqueIdentifiers/Node.cs
+++ tests/Smdn.Fundamental.Uuid/Smdn.Formats.UniversallyUniqueIdentifiers/Node.cs
@@ -82,34 +82,5 @@ namespace Smdn.Formats.UniversallyUniqueIdentifiers {
       Assert.IsFalse(node.TryWriteBytes(stackalloc byte[1]), "length 1");
       Assert.IsFalse(node.TryWriteBytes(stackalloc byte[0]), "length 0");
     }
-
-    [Test]
-    public void TestToString()
-    {
-      var regexFormat_X = new Regex("^[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}$");
-      var regexFormat_x = new Regex("^[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}$");
-      var regexFormat_Default = regexFormat_X;
-
-      for (var n = 0; n < 1000; n++) {
-        var node = Node.CreateRandom();
-
-        Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString()), node.ToString());
-        Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString(null)), node.ToString(null));
-        Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString(string.Empty)), node.ToString(string.Empty));
-        Assert.IsTrue(regexFormat_Default.IsMatch(node.ToString(null, formatProvider: null)), node.ToString(null, formatProvider: null));
-        Assert.IsTrue(regexFormat_X.IsMatch(node.ToString("X")), node.ToString("X"));
-        Assert.IsTrue(regexFormat_x.IsMatch(node.ToString("x")), node.ToString("x"));
-      }
-    }
-
-    [Test]
-    public void TestToString_InvalidFormat()
-    {
-      var node = Node.CreateRandom();
-
-      Assert.Throws<FormatException>(() => node.ToString("n"));
-      Assert.Throws<FormatException>(() => node.ToString("xx"));
-      Assert.Throws<FormatException>(() => node.ToString("XX"));
-    }
   }
 }
