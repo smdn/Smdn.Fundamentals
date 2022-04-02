af7730d73ddee92517847949fdb4a1538520bf0a
smdn <smdn@smdn.jp>
2022-04-02 17:11:57 +0900

    implement ISpanFormattable

--- src/Smdn.Fundamental.Uuid/Smdn/Uuid.IFormattable.cs
+++ src/Smdn.Fundamental.Uuid/Smdn/Uuid.IFormattable.cs
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#nullable enable
+
 using System;
 #if !SYSTEM_FORMATTABLESTRING
 using System.Globalization;
@@ -11,11 +13,17 @@ namespace Smdn;
 partial struct Uuid :
 #pragma warning restore IDE0040
   IFormattable
+#if SYSTEM_ISPANFORMATTABLE
+#pragma warning disable SA1001
+  ,
+#pragma warning restore SA1001
+  ISpanFormattable
+#endif
 {
   public string ToString(string format)
     => ToString(format, null);
 
-  public string ToString(string format, IFormatProvider formatProvider)
+  public string ToString(string? format, IFormatProvider? formatProvider)
   {
     if (format == "X") {
       const string xopen = "{";
@@ -78,5 +86,181 @@ partial struct Uuid :
       node.N5
     );
 #endif
+  }
+
+  public bool TryFormat(
+    Span<char> destination,
+    out int charsWritten,
+    ReadOnlySpan<char> format = default,
+#pragma warning disable IDE0060
+    IFormatProvider? provider = null
+#pragma warning restore IDE0060
+  )
+    => TryFormatCore(destination, out charsWritten, format) == TryFormatResult.Success;
+
+  private enum TryFormatResult {
+    Success,
+    InvalidFormat,
+    Failed,
+  }
+
+  private TryFormatResult TryFormatCore(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format)
+  {
+    charsWritten = default;
+
+    const char defaultFormatChar = 'D';
+    char formatChar;
+
+    if (format.Length == 1)
+      formatChar = format[0];
+    else if (format.IsEmpty)
+      formatChar = defaultFormatChar;
+    else
+      return TryFormatResult.InvalidFormat;
+
+    // D: "00000000-0000-0000-0000-000000000000"
+    // B: "{00000000-0000-0000-0000-000000000000}"
+    // P: "(00000000-0000-0000-0000-000000000000)"
+    // N: "00000000000000000000000000000000"
+    // X: {0x00000000,0x0000,0x0000,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}}
+    (
+      bool isValidFormat,
+      bool prefixedHex,
+      char outerOpen,
+      char outerClose,
+      char outerDelimiter,
+      char clockDelimiter,
+      char innerOpen,
+      char innerClose,
+      char innerDelimiter
+    ) = formatChar switch {
+      'D' => (true, false, default, default, '-', default, default, default, default),
+      'B' => (true, false, '{', '}', '-', default, default, default, default),
+      'P' => (true, false, '(', ')', '-', default, default, default, default),
+      'N' => (true, false, default, default, default, default, default, default, default),
+      'X' => (true, true, '{', '}', ',', ',', '{', '}', ','),
+      _ => default,
+    };
+
+    if (!isValidFormat)
+      return TryFormatResult.InvalidFormat;
+
+    if (outerOpen != default) {
+      if (!TryAppendChar(ref destination, ref charsWritten, outerOpen))
+        return TryFormatResult.Failed;
+    }
+
+    if (!TryFormatHex(ref destination, ref charsWritten, time_low, 4, prefixedHex, outerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, time_mid, 2, prefixedHex, outerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, time_hi_and_version, 2, prefixedHex, outerDelimiter))
+      return TryFormatResult.Failed;
+
+    if (innerOpen != default) {
+      if (!TryAppendChar(ref destination, ref charsWritten, innerOpen))
+        return TryFormatResult.Failed;
+    }
+
+    if (!TryFormatHex(ref destination, ref charsWritten, clock_seq_hi_and_reserved, 1, prefixedHex, clockDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, clock_seq_low, 1, prefixedHex, outerDelimiter))
+      return TryFormatResult.Failed;
+
+    if (!TryFormatHex(ref destination, ref charsWritten, node.N0, 1, prefixedHex, innerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, node.N1, 1, prefixedHex, innerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, node.N2, 1, prefixedHex, innerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, node.N3, 1, prefixedHex, innerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, node.N4, 1, prefixedHex, innerDelimiter))
+      return TryFormatResult.Failed;
+    if (!TryFormatHex(ref destination, ref charsWritten, node.N5, 1, prefixedHex, delimiter: default))
+      return TryFormatResult.Failed;
+
+    if (innerClose != default) {
+      if (!TryAppendChar(ref destination, ref charsWritten, innerClose))
+        return TryFormatResult.Failed;
+    }
+
+    if (outerClose != default) {
+      if (!TryAppendChar(ref destination, ref charsWritten, outerClose))
+        return TryFormatResult.Failed;
+    }
+
+    return TryFormatResult.Success;
+
+    static bool TryAppendChar(ref Span<char> destination, ref int charsWritten, char ch)
+    {
+      if (destination.Length < 1)
+        return false;
+
+      destination[0] = ch;
+      destination = destination.Slice(1);
+
+      charsWritten += 1;
+
+      return true;
+    }
+
+    static bool TryFormatHex(
+      ref Span<char> destination,
+      ref int charsWritten,
+      uint value,
+      int valueSize,
+      bool prefixedHex,
+      char delimiter
+    )
+    {
+      if (prefixedHex) {
+        if (destination.Length < 2)
+          return false;
+
+        destination[0] = '0';
+        destination[1] = 'x';
+        destination = destination.Slice(2);
+        charsWritten += 2;
+      }
+
+      var valueFormat = valueSize switch {
+        1 => "x2",
+        2 => "x4",
+        4 => "x8",
+        _ => throw new NotSupportedException($"undefined value size: {valueSize}"),
+      };
+
+#if SYSTEM_ISPANFORMATTABLE
+      if (!value.TryFormat(destination, out var written, valueFormat, provider: null))
+        return false;
+#else
+      var formattedValue = value.ToString(valueFormat, provider: null);
+
+      if (destination.Length < formattedValue.Length)
+        return false;
+
+      var written = 0;
+
+      for (var i = 0; i < formattedValue.Length; i++) {
+        destination[i] = formattedValue[i];
+        written++;
+      }
+#endif
+
+      destination = destination.Slice(written);
+      charsWritten += written;
+
+      if (delimiter != default) {
+        if (destination.Length < 1)
+          return false;
+
+        destination[0] = delimiter;
+        destination = destination.Slice(1);
+        charsWritten += 1;
+      }
+
+      return true;
+    }
   }
 }
--- tests/Smdn.Fundamental.Uuid/Smdn/Uuid.IFormattable.cs
+++ tests/Smdn.Fundamental.Uuid/Smdn/Uuid.IFormattable.cs
@@ -27,31 +27,109 @@ partial class UuidTests {
     Assert.Throws<FormatException>(() => Uuid.Nil.ToString("Z", null));
   }
 
-  [Test]
-  public void TestToString2()
-  {
-    var guid = new Guid("6ba7b810-9dad-11d1-80b4-00c04fd430c8");
-    var uuid = new Uuid("6ba7b810-9dad-11d1-80b4-00c04fd430c8");
 
-    Assert.AreEqual(guid.ToString(), uuid.ToString());
-    Assert.AreEqual(guid.ToString("N"), uuid.ToString("N"), "format = N");
-    Assert.AreEqual(guid.ToString("D"), uuid.ToString("D"), "format = D");
-    Assert.AreEqual(guid.ToString("B"), uuid.ToString("B"), "format = B");
-    Assert.AreEqual(guid.ToString("P"), uuid.ToString("P"), "format = P");
-    Assert.AreEqual(guid.ToString("X"), uuid.ToString("X"), "format = X");
+  private static System.Collections.IEnumerable YieldTestCase_Format()
+  {
+    yield return new object[] { Uuid.RFC4122NamespaceDns, null, "6ba7b810-9dad-11d1-80b4-00c04fd430c8", 36 };
+    yield return new object[] { Uuid.RFC4122NamespaceDns, string.Empty, "6ba7b810-9dad-11d1-80b4-00c04fd430c8", 36 };
+    yield return new object[] { Uuid.RFC4122NamespaceDns, "D", "6ba7b810-9dad-11d1-80b4-00c04fd430c8", 36 };
+    yield return new object[] { Uuid.RFC4122NamespaceDns, "B", "{6ba7b810-9dad-11d1-80b4-00c04fd430c8}", 38 };
+    yield return new object[] { Uuid.RFC4122NamespaceDns, "P", "(6ba7b810-9dad-11d1-80b4-00c04fd430c8)", 38 };
+    yield return new object[] { Uuid.RFC4122NamespaceDns, "N", "6ba7b8109dad11d180b400c04fd430c8", 32 };
+    yield return new object[] { Uuid.RFC4122NamespaceDns, "X", "{0x6ba7b810,0x9dad,0x11d1,{0x80,0xb4,0x00,0xc0,0x4f,0xd4,0x30,0xc8}}", 68 };
+    yield return new object[] { Uuid.Nil, null, "00000000-0000-0000-0000-000000000000", 36 };
+    yield return new object[] { Uuid.Nil, string.Empty, "00000000-0000-0000-0000-000000000000", 36 };
+    yield return new object[] { Uuid.Nil, "D", "00000000-0000-0000-0000-000000000000", 36 };
+    yield return new object[] { Uuid.Nil, "B", "{00000000-0000-0000-0000-000000000000}", 38 };
+    yield return new object[] { Uuid.Nil, "P", "(00000000-0000-0000-0000-000000000000)", 38 };
+    yield return new object[] { Uuid.Nil, "N", "00000000000000000000000000000000", 32 };
+    yield return new object[] { Uuid.Nil, "X", "{0x00000000,0x0000,0x0000,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}}", 68 };
   }
 
-  [Test]
-  public void TestToString3()
+  [TestCaseSource(nameof(YieldTestCase_Format))]
+  public void TestToString(Uuid uuid, string format, string expectedString, int discard)
+    => Assert.AreEqual(expectedString, uuid.ToString(format, formatProvider: null));
+
+  [TestCaseSource(nameof(YieldTestCase_Format))]
+  public void TestTryFormat(Uuid uuid, string format, string expectedString, int expectedCharsWritten)
+  {
+    if (format is null)
+      return;
+
+    var dest = new char[0x100];
+
+    Assert.IsTrue(uuid.TryFormat(dest, out var charsWritten, format.AsSpan(), provider: null), "TryFormat");
+    Assert.AreEqual(expectedCharsWritten, charsWritten, "charsWritten");
+    Assert.AreEqual(expectedString, new string(dest, 0, charsWritten), "TryFormat result");
+  }
+
+  [TestCase(null)]
+  [TestCase("")]
+  [TestCase("N")]
+  [TestCase("D")]
+  [TestCase("B")]
+  [TestCase("P")]
+  [TestCase("X")]
+  public void TestToString_IsAsSameAsGuidToString(string format)
   {
     var guid = new Guid(new byte[] {0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8});
     var uuid = new Uuid(new byte[] {0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8});
 
-    Assert.AreEqual(guid.ToString(), uuid.ToString());
-    Assert.AreEqual(guid.ToString("N"), uuid.ToString("N"), "format = N");
-    Assert.AreEqual(guid.ToString("D"), uuid.ToString("D"), "format = D");
-    Assert.AreEqual(guid.ToString("B"), uuid.ToString("B"), "format = B");
-    Assert.AreEqual(guid.ToString("P"), uuid.ToString("P"), "format = P");
-    Assert.AreEqual(guid.ToString("X"), uuid.ToString("X"), "format = X");
+    Assert.AreEqual(
+      guid.ToString(format, provider: null),
+      uuid.ToString(format, formatProvider: null)
+    );
   }
+
+#if SYSTEM_ISPANFORMATTABLE
+  [TestCase(null)]
+  [TestCase("")]
+  [TestCase("N")]
+  [TestCase("D")]
+  [TestCase("B")]
+  [TestCase("P")]
+  [TestCase("X")]
+  public void TestTryFormat_IsAsSameAsGuidToString(string format)
+  {
+    var guid = new Guid(new byte[] {0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8});
+    var uuid = new Uuid(new byte[] {0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8});
+
+    var destinationGuid = new char[0x100];
+    var destinationUuid = new char[0x100];
+
+    Assert.AreEqual(
+      guid.TryFormat(destinationGuid, out var charsWrittenGuid, format),
+      uuid.TryFormat(destinationUuid, out var charsWrittenUuid, format, provider: null),
+      "TryFormat"
+    );
+
+    Assert.AreEqual(charsWrittenGuid, charsWrittenUuid, "charsWritten");
+    Assert.AreEqual(destinationGuid, destinationUuid, "destination");
+  }
+#endif
+
+  [TestCase("", 36)]
+  [TestCase("D", 36)]
+  [TestCase("B", 38)]
+  [TestCase("P", 38)]
+  [TestCase("N", 32)]
+  [TestCase("X", 68)]
+  public void TestTryFormat_DestinationTooShort(string format, int requiredLength)
+  {
+    for (var length = 0; length < requiredLength; length++) {
+      Assert.IsFalse(Uuid.Nil.TryFormat(new char[length], out _, format.AsSpan(), provider: null), $"format {format}, length {length}");
+    }
+  }
+
+  [TestCase("Q")]
+  [TestCase("D2")]
+  [TestCase("x")]
+  public void TestToString_InvalidFormat(string format)
+    => Assert.Throws<FormatException>(() => Uuid.Nil.ToString(format));
+
+  [TestCase("Q")]
+  [TestCase("D2")]
+  [TestCase("x")]
+  public void TestTryFormat_InvalidFormat(string format)
+    => Assert.IsFalse(Uuid.Nil.TryFormat(new char[0x100], out _, format.AsSpan()));
 }
