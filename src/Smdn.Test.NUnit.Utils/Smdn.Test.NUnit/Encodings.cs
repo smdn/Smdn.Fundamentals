ba3937d8df57b13acf2a3bebf98c0ae078109e19
smdn <smdn@smdn.jp>
2022-04-15 21:25:13 +0900

    rename TestUtils.Encodings to Encodings

--- src/Smdn.Test.NUnit.Utils/Smdn.Test.NUnit/Encodings.cs
+++ src/Smdn.Test.NUnit.Utils/Smdn.Test.NUnit/Encodings.cs
@@ -2,20 +2,18 @@
 // SPDX-License-Identifier: MIT
 using System.Text;
 
-namespace Smdn.Test.NUnit {
-  public static partial class TestUtils {
-    public static class Encodings {
-      public static readonly Encoding Latin1    = Encoding.GetEncoding("latin1");
-      public static readonly Encoding Jis       = GetEncoding("iso-2022-jp");
-      public static readonly Encoding ShiftJis  = GetEncoding("shift_jis");
-      public static readonly Encoding EucJP     = GetEncoding("euc-jp");
+namespace Smdn.Test.NUnit;
 
-      private static Encoding GetEncoding(string name)
+public static class Encodings {
+  public static readonly Encoding Latin1    = Encoding.GetEncoding("latin1");
+  public static readonly Encoding Jis       = GetEncoding("iso-2022-jp");
+  public static readonly Encoding ShiftJis  = GetEncoding("shift_jis");
+  public static readonly Encoding EucJP     = GetEncoding("euc-jp");
+
+  private static Encoding GetEncoding(string name)
 #if NETFRAMEWORK
-        => Encoding.GetEncoding(name);
+    => Encoding.GetEncoding(name);
 #else
-        => CodePagesEncodingProvider.Instance.GetEncoding(name);
+    => CodePagesEncodingProvider.Instance.GetEncoding(name);
 #endif
-    }
-  }
 }
