bf54fce4066eecc89fd3be901257361c0d63b3f7
smdn <smdn@smdn.jp>
2022-04-15 21:29:02 +0900

    use Encoding.Latin1 if available

--- src/Smdn.Test.NUnit.Utils/Smdn.Test.NUnit/Encodings.cs
+++ src/Smdn.Test.NUnit.Utils/Smdn.Test.NUnit/Encodings.cs
@@ -1,11 +1,20 @@
 // SPDX-FileCopyrightText: 2020 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NET5_0_OR_GREATER
+#define SYSTEM_TEXT_ENCODING_LATIN1
+#endif
+
 using System.Text;
 
 namespace Smdn.Test.NUnit;
 
 public static class Encodings {
-  public static Encoding Latin1 => Encoding.GetEncoding("latin1");
+  public static Encoding Latin1 =>
+#if SYSTEM_TEXT_ENCODING_LATIN1
+    Encoding.Latin1;
+#else
+    Encoding.GetEncoding("latin1");
+#endif
   public static Encoding Jis => GetEncoding("iso-2022-jp");
   public static Encoding ShiftJis => GetEncoding("shift_jis");
   public static Encoding EucJP => GetEncoding("euc-jp");
