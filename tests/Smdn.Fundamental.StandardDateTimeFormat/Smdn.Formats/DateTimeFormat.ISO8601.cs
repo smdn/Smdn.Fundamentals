aa7184a60a1274ab4f9f341b4d599f1abf8cadcc
smdn <smdn@smdn.jp>
2022-04-26 21:12:01 +0900

    add test cases

--- tests/Smdn.Fundamental.StandardDateTimeFormat/Smdn.Formats/DateTimeFormat.ISO8601.cs
+++ tests/Smdn.Fundamental.StandardDateTimeFormat/Smdn.Formats/DateTimeFormat.ISO8601.cs
@@ -8,9 +8,20 @@ namespace Smdn.Formats;
 
 partial class DateTimeFormatTests {
   [Test]
-  public void ToISO8601DateTimeString_ReturnValueEqualsToToW3CDateTimeString()
+  public void ToISO8601DateTimeString_DateTime_ReturnValueEqualsToToW3CDateTimeString()
   {
     var dtm = new DateTime(2008, 2, 25, 15, 1, 12, DateTimeKind.Utc);
+
+    Assert.AreEqual(
+      DateTimeFormat.ToW3CDateTimeString(dtm),
+      DateTimeFormat.ToISO8601DateTimeString(dtm)
+    );
+  }
+
+  [Test]
+  public void ToISO8601DateTimeString_DateTimeOffset_ReturnValueEqualsToToW3CDateTimeString()
+  {
+    var dtm = new DateTimeOffset(2008, 2, 25, 15, 1, 12, TimeSpan.FromHours(+9));
 
     Assert.AreEqual(
       DateTimeFormat.ToW3CDateTimeString(dtm),
@@ -23,7 +34,19 @@ partial class DateTimeFormatTests {
   [TestCase("2008-04-11T12:34:56Z")]
   [TestCase("2008-04-11T12:34Z")]
 
-  public void FromISO8601DateTimeString_ReturnValueEqualsToFromW3CDateTimeString(string s)
+  public void FromISO8601DateTimeString_DateTime_ReturnValueEqualsToFromW3CDateTimeString(string s)
+    => Assert.AreEqual(
+      DateTimeFormat.FromW3CDateTimeString(s),
+      DateTimeFormat.FromISO8601DateTimeString(s)
+    );
+
+  [TestCase("2008-04-11T12:34:56.7893333 +09:00")]
+  [TestCase("2008-04-11T12:34:56.7893333 -04:00")]
+  [TestCase("2008-04-11T12:34:56.7893333 -12:45")]
+  [TestCase("2008-04-11T12:34:56.789 +09:00")]
+  [TestCase("2008-04-11T12:34:56 +09:00")]
+  [TestCase("2008-04-11T12:34 +09:00")]
+  public void FromISO8601DateTimeString_DateTimeOffset_ReturnValueEqualsToFromW3CDateTimeString(string s)
     => Assert.AreEqual(
       DateTimeFormat.FromW3CDateTimeString(s),
       DateTimeFormat.FromISO8601DateTimeString(s)
