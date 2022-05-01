d8cc13ee4bceafbd08aaab64f5c977e47fc1b626
smdn <smdn@smdn.jp>
2022-05-02 01:59:20 +0900

    add ISO8601DateTimeFormats.ToWeekDateString

--- src/Smdn.Fundamental.StandardDateTimeFormat/Smdn.Formats.DateAndTime/ISO8601DateTimeFormats.cs
+++ src/Smdn.Fundamental.StandardDateTimeFormat/Smdn.Formats.DateAndTime/ISO8601DateTimeFormats.cs
@@ -1,24 +1,51 @@
 // SPDX-FileCopyrightText: 2009 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+#define SYSTEM_GLOBALIZATION_ISOWEEK
+#endif
+#if NET6_0_OR_GREATER
+#define SYSTEM_DATEONLY
+#endif
+
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Smdn.Formats.DateAndTime;
 
-internal static class ISO8601DateTimeFormats {
-  public static string ToString(DateTime dateTime)
+public static class ISO8601DateTimeFormats {
+  internal static string ToString(DateTime dateTime)
     => dateTime.ToString("o");
 
-  public static string ToString(DateTimeOffset dateTimeOffset)
+  internal static string ToString(DateTimeOffset dateTimeOffset)
     => dateTimeOffset.ToString("o");
 
-  public static DateTime ParseDateTime(string s)
+  internal static DateTime ParseDateTime(string s)
     => DateAndTimeParser.ParseDateTime(s, W3CDateTimeFormats.FormatStrings, timeZoneDefinitions);
 
-  public static DateTimeOffset ParseDateTimeOffset(string s)
+  internal static DateTimeOffset ParseDateTimeOffset(string s)
     => DateAndTimeParser.ParseDateTimeOffset(s, W3CDateTimeFormats.FormatStrings, timeZoneDefinitions);
 
   private static readonly IReadOnlyList<TimeZoneDefinition> timeZoneDefinitions = new[] {
     new UniversalTimeZoneDefinition("Z"),
   };
+
+#if SYSTEM_GLOBALIZATION_ISOWEEK
+#if SYSTEM_DATEONLY
+  public static string ToWeekDateString(DateOnly date)
+    => ToWeekDateString(date.ToDateTime(default(TimeOnly), DateTimeKind.Unspecified));
+#endif
+
+  public static string ToWeekDateString(DateTimeOffset date)
+    => ToWeekDateString(date.Date);
+
+  public static string ToWeekDateString(DateTime date)
+    => string.Format(
+      CultureInfo.InvariantCulture.NumberFormat,
+      "{0:D4}-W{1:D2}-{2:D2}",
+      ISOWeek.GetYear(date),
+      ISOWeek.GetWeekOfYear(date),
+      date.DayOfWeek == DayOfWeek.Sunday ? 7 : (int)date.DayOfWeek
+    );
+#endif
 }
--- tests/Smdn.Fundamental.StandardDateTimeFormat/Smdn.Formats.DateAndTime/ISO8601DateTimeFormats.cs
+++ tests/Smdn.Fundamental.StandardDateTimeFormat/Smdn.Formats.DateAndTime/ISO8601DateTimeFormats.cs
@@ -1,7 +1,17 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+#define SYSTEM_GLOBALIZATION_ISOWEEK
+#endif
+#if NET6_0_OR_GREATER
+#define SYSTEM_DATEONLY
+#endif
+
 using System;
 using System.Collections;
+#if SYSTEM_GLOBALIZATION_ISOWEEK
+using System.Globalization;
+#endif
 
 using NUnit.Framework;
 
@@ -31,4 +41,62 @@ public class ISO8601DateTimeFormatsTests {
   [TestCaseSource(nameof(YieldTestCases_ParseCommon_InvalidFormat))]
   public void ParseDateTimeOffset_InvalidFormat(string s)
     => Assert.Throws<FormatException>(() => DateTimeFormat.FromISO8601DateTimeOffsetString(s)); // TODO: test ISO8601DateTimeFormats.ParseDateTimeOffset
+
+#if SYSTEM_GLOBALIZATION_ISOWEEK
+  private static IEnumerable YieldTestCases_ToWeekDateString(bool time, bool offset)
+  {
+    string t = time ? "T00:00:00.0000000" : string.Empty;
+    string tz = time && offset ? "+00:00" : string.Empty;
+
+    yield return new[] { $"2019-12-29{t}{tz}", "2019-W52-07" };
+    yield return new[] { $"2019-12-30{t}{tz}", "2020-W01-01" };
+    yield return new[] { $"2020-01-01{t}{tz}", "2020-W01-03" };
+    yield return new[] { $"2020-06-30{t}{tz}", "2020-W27-02" };
+    yield return new[] { $"2020-12-31{t}{tz}", "2020-W53-04" };
+    yield return new[] { $"2021-01-03{t}{tz}", "2020-W53-07" };
+    yield return new[] { $"2021-12-31{t}{tz}", "2021-W52-05" };
+    yield return new[] { $"2022-05-01{t}{tz}", "2022-W17-07" };
+
+    if (time && offset) {
+      yield return new[] { "2020-01-01T00:00:00.0000000Z", "2020-W01-03" };
+      yield return new[] { "2020-01-01T00:00:00.0000000+00:00", "2020-W01-03" };
+      yield return new[] { "2020-01-01T00:00:00.0000000-04:00", "2020-W01-03" };
+      yield return new[] { "2020-01-01T00:00:00.0000000+12:45", "2020-W01-03" };
+      yield return new[] { "2020-01-01T00:00:00.0000000+09:00", "2020-W01-03" };
+    }
+  }
+
+  private static IEnumerable YieldTestCases_ToWeekDateString_DateTime()
+    => YieldTestCases_ToWeekDateString(time: true, offset: false);
+
+  private static IEnumerable YieldTestCases_ToWeekDateString_DateTimeOffset()
+    => YieldTestCases_ToWeekDateString(time: true, offset: true);
+
+  [TestCaseSource(nameof(YieldTestCases_ToWeekDateString_DateTime))]
+  public void ToWeekDateString_DateTime(string dateTime, string expected)
+    => Assert.AreEqual(
+      expected,
+      ISO8601DateTimeFormats.ToWeekDateString(DateTime.ParseExact(dateTime, "o", null))
+    );
+
+  [TestCaseSource(nameof(YieldTestCases_ToWeekDateString_DateTimeOffset))]
+  public void ToWeekDateString_DateTimeOffset(string dateTime, string expected)
+    => Assert.AreEqual(
+      expected,
+      ISO8601DateTimeFormats.ToWeekDateString(DateTimeOffset.ParseExact(dateTime, "o", null))
+    );
+
+#if SYSTEM_DATEONLY
+  private static IEnumerable YieldTestCases_ToWeekDateString_DateOnly()
+    => YieldTestCases_ToWeekDateString(time: false, offset: false);
+
+  [TestCaseSource(nameof(YieldTestCases_ToWeekDateString_DateOnly))]
+  public void ToWeekDateString_DateOnly(string dateTime, string expected)
+    => Assert.AreEqual(
+      expected,
+      ISO8601DateTimeFormats.ToWeekDateString(DateOnly.ParseExact(dateTime, "o", null))
+    );
+#endif
+
+#endif
 }
