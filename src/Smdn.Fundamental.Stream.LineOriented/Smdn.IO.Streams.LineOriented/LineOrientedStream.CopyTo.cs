689a5bf3b6678ece6bf030db3c02e5d0286f3599
smdn <smdn@smdn.jp>
2022-05-12 23:01:11 +0900

    override CopyTo(Stream, int)

--- src/Smdn.Fundamental.Stream.LineOriented/Smdn.IO.Streams.LineOriented/LineOrientedStream.CopyTo.cs
+++ src/Smdn.Fundamental.Stream.LineOriented/Smdn.IO.Streams.LineOriented/LineOrientedStream.CopyTo.cs
@@ -1,5 +1,9 @@
 // SPDX-FileCopyrightText: 2009 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER || NET5_0_OR_GREATER
+#define SYSTEM_IO_STREAM_COPYTO_VIRTUAL
+#endif
+
 using System;
 using System.IO;
 using System.Threading;
@@ -11,6 +15,34 @@ namespace Smdn.IO.Streams.LineOriented;
 partial class LineOrientedStream {
 #pragma warning restore IDE0040
   private const int CopyToDefaultBufferSize = 1;
+
+#if SYSTEM_IO_STREAM_COPYTO_VIRTUAL
+  /// <param name="destination">The destination stream.</param>
+  /// <param name="bufferSize">The value of this parameter does not affect to the behavior of the method.</param>
+  public override void CopyTo(
+    Stream destination,
+    int bufferSize = CopyToDefaultBufferSize
+  )
+  {
+    CheckDisposed();
+
+#if SYSTEM_IO_STREAM_VALIDATECOPYTOARGUMENTS
+    ValidateCopyToArguments(destination, bufferSize);
+#else
+    if (destination is null)
+      throw new ArgumentNullException(nameof(destination));
+    if (!destination.CanWrite)
+      throw new NotSupportedException("The destination stream does not support writing."); // ExceptionUtils.CreateArgumentMustBeWritableStream(nameof(destination));
+    if (bufferSize <= 0)
+      throw ExceptionUtils.CreateArgumentMustBeNonZeroPositive(nameof(bufferSize), bufferSize);
+#endif
+
+    ReadToStream(
+      destination: destination,
+      bytesToRead: null // read to end
+    );
+  }
+#endif
 
 #pragma warning disable CS0109
   public new Task CopyToAsync(
