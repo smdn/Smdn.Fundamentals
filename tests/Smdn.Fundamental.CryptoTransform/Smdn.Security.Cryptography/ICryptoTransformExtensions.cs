a4faacebd849be5c5bc2d1c8509d5d64abd3b7fd
smdn <smdn@smdn.jp>
2022-05-15 20:07:32 +0900

    reformat

--- tests/Smdn.Fundamental.CryptoTransform/Smdn.Security.Cryptography/ICryptoTransformExtensions.cs
+++ tests/Smdn.Fundamental.CryptoTransform/Smdn.Security.Cryptography/ICryptoTransformExtensions.cs
@@ -7,160 +7,170 @@ using System.Text;
 using System.Security.Cryptography;
 using NUnit.Framework;
 
-namespace Smdn.Security.Cryptography {
-  [TestFixture]
-  public class ICryptoTransformExtensionsTests {
-    [Test]
-    public void TestTransformBytes()
-    {
-      var buffer = new byte[] {0xff, 0xff, 0x61, 0x62, 0x63, 0x64, 0x65, 0xff, 0xff};
-      var expected = new byte[] {0x59, 0x57, 0x4a, 0x6a, 0x5a, 0x47, 0x55, 0x3d};
+namespace Smdn.Security.Cryptography;
 
-      using var transform = Smdn.Formats.Base64.CreateToBase64Transform();
+[TestFixture]
+public class ICryptoTransformExtensionsTests {
+  [Test]
+  public void TestTransformBytes()
+  {
+    var buffer = new byte[] {0xff, 0xff, 0x61, 0x62, 0x63, 0x64, 0x65, 0xff, 0xff};
+    var expected = new byte[] {0x59, 0x57, 0x4a, 0x6a, 0x5a, 0x47, 0x55, 0x3d};
 
-      Assert.AreEqual(expected,
-                      ICryptoTransformExtensions.TransformBytes(transform, buffer.Skip(2).Take(5).ToArray()));
-      Assert.AreEqual(expected,
-                      ICryptoTransformExtensions.TransformBytes(transform, buffer, 2, 5));
-    }
+    using var transform = Smdn.Formats.Base64.CreateToBase64Transform();
 
-    [TestCase("A",   "QQ==")]
-    [TestCase("AA",  "QUE=")]
-    [TestCase("AAA", "QUFB")]
-    public void TestTransformBytes_InputShorterThanInputBlockSize(string input, string output)
-    {
-      using var transform = Smdn.Formats.Base64.CreateToBase64Transform();
+    Assert.AreEqual(
+      expected,
+      ICryptoTransformExtensions.TransformBytes(transform, buffer.Skip(2).Take(5).ToArray())
+    );
+    Assert.AreEqual(
+      expected,
+      ICryptoTransformExtensions.TransformBytes(transform, buffer, 2, 5)
+    );
+  }
 
-      Assert.AreEqual(
-        output,
-        Encoding.ASCII.GetString(
-          ICryptoTransformExtensions.TransformBytes(transform, Encoding.ASCII.GetBytes(input))
-        ),
-        $"input = {input}"
-      );
-    }
+  [TestCase("A",   "QQ==")]
+  [TestCase("AA",  "QUE=")]
+  [TestCase("AAA", "QUFB")]
+  public void TestTransformBytes_InputShorterThanInputBlockSize(string input, string output)
+  {
+    using var transform = Smdn.Formats.Base64.CreateToBase64Transform();
 
-    [Test]
-    public void TestTransformBytesArgumentException()
-    {
-      var buffer = new byte[] {0xff, 0xff, 0x61, 0x62, 0x63, 0x64, 0x65, 0xff, 0xff};
+    Assert.AreEqual(
+      output,
+      Encoding.ASCII.GetString(
+        ICryptoTransformExtensions.TransformBytes(transform, Encoding.ASCII.GetBytes(input))
+      ),
+      $"input = {input}"
+    );
+  }
 
-      using (var transform = Smdn.Formats.Base64.CreateToBase64Transform()) {
-        Assert.Throws<ArgumentNullException>(() => ICryptoTransformExtensions.TransformBytes(null, buffer, 0, 9));
+  [Test]
+  public void TestTransformBytesArgumentException()
+  {
+    var buffer = new byte[] {0xff, 0xff, 0x61, 0x62, 0x63, 0x64, 0x65, 0xff, 0xff};
 
-        Assert.Throws<ArgumentNullException>(() => ICryptoTransformExtensions.TransformBytes(transform, null, 0, 9));
+    using var transform = Smdn.Formats.Base64.CreateToBase64Transform();
 
-        Assert.Throws<ArgumentOutOfRangeException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, -1, 10));
+    Assert.Throws<ArgumentNullException>(() => ICryptoTransformExtensions.TransformBytes(null, buffer, 0, 9));
 
-        Assert.Throws<ArgumentOutOfRangeException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 10, -1));
+    Assert.Throws<ArgumentNullException>(() => ICryptoTransformExtensions.TransformBytes(transform, null, 0, 9));
 
-        Assert.Throws<ArgumentException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 1, 9));
+    Assert.Throws<ArgumentOutOfRangeException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, -1, 10));
 
-        Assert.Throws<ArgumentException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 9, 1));
-      }
-    }
+    Assert.Throws<ArgumentOutOfRangeException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 10, -1));
 
-    private byte[] TransformByCryptoStream(HashAlgorithm algorithm, byte[] bytes)
-    {
-      algorithm.Initialize();
+    Assert.Throws<ArgumentException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 1, 9));
 
-      return TransformByCryptoStream((ICryptoTransform)algorithm, bytes);
-    }
+    Assert.Throws<ArgumentException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 9, 1));
+  }
 
-    private byte[] TransformByCryptoStream(SymmetricAlgorithm algorithm, byte[] bytes, bool encrypt)
-    {
-      if (encrypt)
-        return TransformByCryptoStream(algorithm.CreateEncryptor(), bytes);
-      else
-        return TransformByCryptoStream(algorithm.CreateDecryptor(), bytes);
-    }
+  private byte[] TransformByCryptoStream(HashAlgorithm algorithm, byte[] bytes)
+  {
+    algorithm.Initialize();
 
-    private byte[] TransformByCryptoStream(ICryptoTransform transform, byte[] bytes)
-    {
-      using (var memoryStream = new MemoryStream()) {
-        using (var cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write)) {
-          cryptoStream.Write(bytes, 0, bytes.Length);
-        }
+    return TransformByCryptoStream((ICryptoTransform)algorithm, bytes);
+  }
 
-        return memoryStream.ToArray();
-      }
-    }
+  private byte[] TransformByCryptoStream(SymmetricAlgorithm algorithm, byte[] bytes, bool encrypt)
+  {
+    if (encrypt)
+      return TransformByCryptoStream(algorithm.CreateEncryptor(), bytes);
+    else
+      return TransformByCryptoStream(algorithm.CreateDecryptor(), bytes);
+  }
+
+  private byte[] TransformByCryptoStream(ICryptoTransform transform, byte[] bytes)
+  {
+    using var memoryStream = new MemoryStream();
+    using var cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write);
+
+    cryptoStream.Write(bytes, 0, bytes.Length);
+    cryptoStream.Close();
+
+    return memoryStream.ToArray();
+  }
 
 #if NETFRAMEWORK || NETCOREAPP2_0_OR_GREATER || NET5_O_OR_GREATER
-    [Test]
-    public void TestTranformBytesWithHashAlgorithm()
-    {
-      var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
+  [Test]
+  public void TestTranformBytesWithHashAlgorithm()
+  {
+    var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
 
-      var hashAlgorithms = new HashAlgorithm[] {
+    var hashAlgorithms = new HashAlgorithm[] {
 #pragma warning disable CA5350, CA5351 // uses a weak/broken cryptographic algorithm
-        new HMACMD5(),
-        new HMACSHA512(),
-        MD5.Create(),
+      new HMACMD5(),
+      new HMACSHA512(),
+      MD5.Create(),
 #pragma warning disable SYSLIB0021
-        new SHA512Managed(),
+      new SHA512Managed(),
 #pragma warning restore SYSLIB0021
 #if NETFRAMEWORK
-        new RIPEMD160Managed(),
+      new RIPEMD160Managed(),
 #endif
 #pragma warning restore CA5350, CA5351
-      };
+    };
 
-      foreach (var hashAlgorithm in hashAlgorithms) {
-        hashAlgorithm.Initialize();
+    foreach (var hashAlgorithm in hashAlgorithms) {
+      hashAlgorithm.Initialize();
 
-        Assert.AreEqual(hashAlgorithm.TransformBytes(bytes),
-                        TransformByCryptoStream(hashAlgorithm, bytes),
-                        "HashAlgorithm: {0}",
-                        hashAlgorithm.GetType());
-      }
+      Assert.AreEqual(
+        hashAlgorithm.TransformBytes(bytes),
+        TransformByCryptoStream(hashAlgorithm, bytes),
+        "HashAlgorithm: {0}",
+        hashAlgorithm.GetType()
+      );
     }
+  }
 
-    [Test]
-    public void TestTranformBytesWithSymmetricAlgorithm()
-    {
-      var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
+  [Test]
+  public void TestTranformBytesWithSymmetricAlgorithm()
+  {
+    var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
 
-      var symmetricAlgorithms = new SymmetricAlgorithm[] {
+    var symmetricAlgorithms = new SymmetricAlgorithm[] {
 #pragma warning disable CA5350, CA5351 // uses a weak/broken cryptographic algorithm
 #pragma warning disable SYSLIB0022
-        Rijndael.Create(),
+      Rijndael.Create(),
 #pragma warning restore SYSLIB0022
-        DES.Create(),
-        TripleDES.Create(),
-        RC2.Create(),
+      DES.Create(),
+      TripleDES.Create(),
+      RC2.Create(),
 #pragma warning restore CA5350, CA5351
-      };
+    };
 
-      foreach (var symmetricAlgorithm in symmetricAlgorithms) {
-        var rng = RandomNumberGenerator.Create();
-        var nonce = new byte[symmetricAlgorithm.KeySize / 8];
+    foreach (var symmetricAlgorithm in symmetricAlgorithms) {
+      var rng = RandomNumberGenerator.Create();
+      var nonce = new byte[symmetricAlgorithm.KeySize / 8];
 
-        rng.GetBytes(nonce);
+      rng.GetBytes(nonce);
 
-        symmetricAlgorithm.Padding = PaddingMode.Zeros;
-        symmetricAlgorithm.Key = nonce;
-        symmetricAlgorithm.GenerateIV();
+      symmetricAlgorithm.Padding = PaddingMode.Zeros;
+      symmetricAlgorithm.Key = nonce;
+      symmetricAlgorithm.GenerateIV();
 
-        symmetricAlgorithm.Clear();
+      symmetricAlgorithm.Clear();
 
-        var encrypted = symmetricAlgorithm.CreateEncryptor().TransformBytes(bytes);
+      var encrypted = symmetricAlgorithm.CreateEncryptor().TransformBytes(bytes);
 
-        Assert.AreEqual(BitConverter.ToString(TransformByCryptoStream(symmetricAlgorithm, bytes, true)),
-                        BitConverter.ToString(encrypted),
-                        "SymmetricAlgorithm (Encrypt): {0}",
-                        symmetricAlgorithm.GetType());
+      Assert.AreEqual(
+        BitConverter.ToString(TransformByCryptoStream(symmetricAlgorithm, bytes, true)),
+        BitConverter.ToString(encrypted),
+        "SymmetricAlgorithm (Encrypt): {0}",
+        symmetricAlgorithm.GetType()
+      );
 
-        symmetricAlgorithm.Clear();
+      symmetricAlgorithm.Clear();
 
-        var decrypted = Encoding.ASCII.GetString(symmetricAlgorithm.CreateDecryptor().TransformBytes(encrypted));
+      var decrypted = Encoding.ASCII.GetString(symmetricAlgorithm.CreateDecryptor().TransformBytes(encrypted));
 
-        Assert.AreEqual(Encoding.ASCII.GetString(TransformByCryptoStream(symmetricAlgorithm, encrypted, false)),
-                        decrypted,
-                        "SymmetricAlgorithm (Decrypt): {0}",
-                        symmetricAlgorithm.GetType());
-      }
+      Assert.AreEqual(
+        Encoding.ASCII.GetString(TransformByCryptoStream(symmetricAlgorithm, encrypted, false)),
+        decrypted,
+        "SymmetricAlgorithm (Decrypt): {0}",
+        symmetricAlgorithm.GetType()
+      );
     }
-#endif
   }
+#endif
 }
