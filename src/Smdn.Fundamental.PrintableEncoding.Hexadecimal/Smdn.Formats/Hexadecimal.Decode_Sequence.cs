f9d5ac8f2fac169f49cdb9e7df0f5089cd98215d
smdn <smdn@smdn.jp>
2022-05-15 19:42:55 +0900

    add overloads TryDecode(ArraySegment, ArraySegment)

--- src/Smdn.Fundamental.PrintableEncoding.Hexadecimal/Smdn.Formats/Hexadecimal.Decode_Sequence.cs
+++ src/Smdn.Fundamental.PrintableEncoding.Hexadecimal/Smdn.Formats/Hexadecimal.Decode_Sequence.cs
@@ -7,6 +7,27 @@ namespace Smdn.Formats;
 #pragma warning disable IDE0040
 partial class Hexadecimal {
 #pragma warning restore IDE0040
+  public static bool TryDecodeUpperCase(ArraySegment<char> upperCaseDataSequence, ArraySegment<byte> destination, out int decodedLength)
+#if SYSTEM_READONLYSPAN
+    => TryDecode(upperCaseDataSequence.AsSpan(), destination.AsSpan(), allowUpperCase: true, allowLowerCase: false, out decodedLength);
+#else
+    => TryDecode(upperCaseDataSequence, destination, allowUpperCase: true, allowLowerCase: false, out decodedLength);
+#endif
+
+  public static bool TryDecodeLowerCase(ArraySegment<char> lowerCaseDataSequence, ArraySegment<byte> destination, out int decodedLength)
+#if SYSTEM_READONLYSPAN
+    => TryDecode(lowerCaseDataSequence.AsSpan(), destination.AsSpan(), allowUpperCase: false, allowLowerCase: true, out decodedLength);
+#else
+    => TryDecode(lowerCaseDataSequence, destination, allowUpperCase: false, allowLowerCase: true, out decodedLength);
+#endif
+
+  public static bool TryDecode(ArraySegment<char> dataSequence, ArraySegment<byte> destination, out int decodedLength)
+#if SYSTEM_READONLYSPAN
+    => TryDecode(dataSequence.AsSpan(), destination.AsSpan(), allowUpperCase: true, allowLowerCase: true, out decodedLength);
+#else
+    => TryDecode(dataSequence, destination, allowUpperCase: true, allowLowerCase: true, out decodedLength);
+#endif
+
 #if SYSTEM_READONLYSPAN
   public static bool TryDecodeUpperCase(ReadOnlySpan<char> upperCaseDataSequence, Span<byte> destination, out int decodedLength)
     => TryDecode(upperCaseDataSequence, destination, allowUpperCase: true, allowLowerCase: false, out decodedLength);
@@ -46,5 +67,35 @@ partial class Hexadecimal {
 
     return true;
   }
-#endif
+#else
+  internal static bool TryDecode(ArraySegment<char> dataSequence, ArraySegment<byte> destination, bool allowUpperCase, bool allowLowerCase, out int decodedLength)
+  {
+    decodedLength = 0;
+
+    var length = dataSequence.Count;
+
+    if (length == 0)
+      return true;
+
+    if ((length & 0x1) != 0)
+      return false; // incorrect form
+
+    length >>= 1;
+
+    if (destination.Count < length)
+      return false; // destination too short
+
+    for (var i = 0; i < length; i++) {
+      if (!TryDecode(dataSequence, allowUpperCase, allowLowerCase, out byte decodedData))
+        return false;
+
+      destination.Array[destination.Offset + i] = decodedData;
+
+      dataSequence = new(dataSequence.Array, dataSequence.Offset + 2, dataSequence.Count - 2);
+      decodedLength++;
+    }
+
+    return true;
+  }
+  #endif
 }
--- tests/Smdn.Fundamental.PrintableEncoding.Hexadecimal/Smdn.Formats/Hexadecimal.Decode_Sequence.cs
+++ tests/Smdn.Fundamental.PrintableEncoding.Hexadecimal/Smdn.Formats/Hexadecimal.Decode_Sequence.cs
@@ -8,9 +8,33 @@ using Is = Smdn.Test.NUnit.Constraints.Buffers.Is;
 namespace Smdn.Formats;
 
 partial class HexadecimalTests {
+  private static ArraySegment<char> ToArraySegment(string input)
+    => new(input.ToCharArray());
+
+  [Test]
+  public void TryDecode_OfCharArraySegmentSequence_IncorrectForm()
+  {
+    var destination = new ArraySegment<byte>(new byte[16]);
+
+    Assert.IsFalse(Hexadecimal.TryDecode(ToArraySegment("0"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeUpperCase(ToArraySegment("0"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeLowerCase(ToArraySegment("0"), destination, out _));
+
+    Assert.IsFalse(Hexadecimal.TryDecode(ToArraySegment("012"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeUpperCase(ToArraySegment("012"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeLowerCase(ToArraySegment("012"), destination, out _));
+
+    Assert.IsFalse(Hexadecimal.TryDecode(ToArraySegment("XX"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeUpperCase(ToArraySegment("XX"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeLowerCase(ToArraySegment("XX"), destination, out _));
+
+    Assert.IsFalse(Hexadecimal.TryDecodeUpperCase(ToArraySegment("ab"), destination, out _));
+    Assert.IsFalse(Hexadecimal.TryDecodeLowerCase(ToArraySegment("AB"), destination, out _));
+  }
+
 #if SYSTEM_READONLYSPAN
   [Test]
-  public void TryDecode_OfCharSequence_IncorrectForm()
+  public void TryDecode_OfReadOnlyCharSpanSequence_IncorrectForm()
   {
     var destination = new byte[16];
 
@@ -29,9 +53,27 @@ partial class HexadecimalTests {
     Assert.IsFalse(Hexadecimal.TryDecodeUpperCase("ab".AsSpan(), destination, out _));
     Assert.IsFalse(Hexadecimal.TryDecodeLowerCase("AB".AsSpan(), destination, out _));
   }
+#endif
 
   [Test]
-  public void TryDecode_OfCharSequence_UpperCase()
+  public void TryDecode_OfCharArraySegmentSequence_UpperCase()
+  {
+    const string dataSequence = "0123456789ABCDEF";
+    var expected = new byte[] { 0x00, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x00 };
+    var destination = new byte[expected.Length];
+
+    Assert.IsTrue(Hexadecimal.TryDecode(ToArraySegment(dataSequence), new ArraySegment<byte>(destination, 1, 8), out var decodedLength0), nameof(Hexadecimal.TryDecode));
+    Assert.AreEqual(8, decodedLength0, nameof(decodedLength0));
+    Assert.That(destination, Is.EqualTo(expected), nameof(destination));
+
+    Assert.IsTrue(Hexadecimal.TryDecodeUpperCase(ToArraySegment(dataSequence), new ArraySegment<byte>(destination, 1, 8), out var decodedLength1), nameof(Hexadecimal.TryDecodeUpperCase));
+    Assert.AreEqual(8, decodedLength1, nameof(decodedLength1));
+    Assert.That(destination, Is.EqualTo(expected), nameof(destination));
+  }
+
+#if SYSTEM_READONLYSPAN
+  [Test]
+  public void TryDecode_OfReadOnlyCharSpanSequence_UpperCase()
   {
     const string dataSequence = "0123456789ABCDEF";
     var expected = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
@@ -45,9 +87,27 @@ partial class HexadecimalTests {
     Assert.AreEqual(expected.Length, decodedLength1, nameof(decodedLength1));
     Assert.That(destination, Is.EqualTo(expected), nameof(destination));
   }
+#endif
 
   [Test]
-  public void TryDecode_OfCharSequence_LowerCase()
+  public void TryDecode_OfCharArraySegmentSequence_LowerCase()
+  {
+    const string dataSequence = "0123456789abcdef";
+    var expected = new byte[] { 0x00, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x00 };
+    var destination = new byte[expected.Length];
+
+    Assert.IsTrue(Hexadecimal.TryDecode(ToArraySegment(dataSequence), new ArraySegment<byte>(destination, 1, 8), out var decodedLength0), nameof(Hexadecimal.TryDecode));
+    Assert.AreEqual(8, decodedLength0, nameof(decodedLength0));
+    Assert.That(destination, Is.EqualTo(expected), nameof(destination));
+
+    Assert.IsTrue(Hexadecimal.TryDecodeLowerCase(ToArraySegment(dataSequence), new ArraySegment<byte>(destination, 1, 8), out var decodedLength1), nameof(Hexadecimal.TryDecodeLowerCase));
+    Assert.AreEqual(8, decodedLength1, nameof(decodedLength1));
+    Assert.That(destination, Is.EqualTo(expected), nameof(destination));
+  }
+
+#if SYSTEM_READONLYSPAN
+  [Test]
+  public void TryDecode_OfReadOnlyCharSpanSequence_LowerCase()
   {
     const string dataSequence = "0123456789abcdef";
     var expected = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
@@ -61,9 +121,24 @@ partial class HexadecimalTests {
     Assert.AreEqual(expected.Length, decodedLength1, nameof(decodedLength1));
     Assert.That(destination, Is.EqualTo(expected), nameof(destination));
   }
+#endif
 
   [Test]
-  public void TryDecode_OfCharSequence_Empty()
+  public void TryDecode_OfCharArraySegmentSequence_Empty()
+  {
+    Assert.IsTrue(Hexadecimal.TryDecode(CreateEmptyArraySegment<char>(), CreateEmptyArraySegment<byte>(), out var decodedLength0), nameof(Hexadecimal.TryDecode));
+    Assert.AreEqual(0, decodedLength0, nameof(decodedLength0));
+
+    Assert.IsTrue(Hexadecimal.TryDecodeUpperCase(CreateEmptyArraySegment<char>(), CreateEmptyArraySegment<byte>(), out var decodedLength1), nameof(Hexadecimal.TryDecodeUpperCase));
+    Assert.AreEqual(0, decodedLength1, nameof(decodedLength1));
+
+    Assert.IsTrue(Hexadecimal.TryDecodeLowerCase(CreateEmptyArraySegment<char>(), CreateEmptyArraySegment<byte>(), out var decodedLength2), nameof(Hexadecimal.TryDecodeLowerCase));
+    Assert.AreEqual(0, decodedLength2, nameof(decodedLength1));
+  }
+
+#if SYSTEM_READONLYSPAN
+  [Test]
+  public void TryDecode_OfReadOnlyCharSpanSequence_Empty()
   {
     Assert.IsTrue(Hexadecimal.TryDecode(string.Empty.AsSpan(), Span<byte>.Empty, out var decodedLength0), nameof(Hexadecimal.TryDecode));
     Assert.AreEqual(0, decodedLength0, nameof(decodedLength0));
@@ -74,9 +149,21 @@ partial class HexadecimalTests {
     Assert.IsTrue(Hexadecimal.TryDecodeLowerCase(string.Empty.AsSpan(), Span<byte>.Empty, out var decodedLength2), nameof(Hexadecimal.TryDecodeLowerCase));
     Assert.AreEqual(0, decodedLength2, nameof(decodedLength1));
   }
+#endif
 
   [Test]
-  public void TryDecode_OfCharSequence_DestinationTooShort()
+  public void TryDecode_OfCharArraySegmentSequence_DestinationTooShort()
+  {
+    const string dataSequence = "0123456789abcdefABCDEF";
+
+    Assert.IsFalse(Hexadecimal.TryDecode(ToArraySegment(dataSequence), CreateEmptyArraySegment<byte>(), out _), nameof(Hexadecimal.TryDecode));
+    Assert.IsFalse(Hexadecimal.TryDecodeUpperCase(ToArraySegment(dataSequence), CreateEmptyArraySegment<byte>(), out _), nameof(Hexadecimal.TryDecodeUpperCase));
+    Assert.IsFalse(Hexadecimal.TryDecodeLowerCase(ToArraySegment(dataSequence), CreateEmptyArraySegment<byte>(), out _), nameof(Hexadecimal.TryDecodeLowerCase));
+  }
+
+#if SYSTEM_READONLYSPAN
+  [Test]
+  public void TryDecode_OfReadOnlyCharSpanSequence_DestinationTooShort()
   {
     const string dataSequence = "0123456789abcdefABCDEF";
 
