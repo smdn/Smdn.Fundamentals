4568afd91d75d7d265746dd2d9cd71db724141c3
smdn <smdn@smdn.jp>
2022-05-15 21:31:18 +0900

    use file-scoped namespace declaration

--- tests/Smdn.Fundamental.Encoding.OctetEncoding/Smdn.Text.Encodings/OctetEncoding.cs
+++ tests/Smdn.Fundamental.Encoding.OctetEncoding/Smdn.Text.Encodings/OctetEncoding.cs
@@ -5,31 +5,31 @@ using System.Text;
 
 using NUnit.Framework;
 
-namespace Smdn.Text.Encodings {
-  [TestFixture]
-  public class OctetEncodingTests {
-    [Test]
-    public void TestEncodeTransfer7BitValidChar()
-    {
-      OctetEncoding.SevenBits.GetBytes("0004 append \"INBOX\" (\\Seen) {33}\r\n\x00\x20\x40\x60\x7f");
-    }
+namespace Smdn.Text.Encodings;
 
-    [Test]
-    public void TestEncodeTransfer8BitValidChar()
-    {
-      OctetEncoding.EightBits.GetBytes("0004 append \"INBOX\" (\\Seen) {33}\r\n\x00\x20\x40\x60\x80\xa0\xc0\xe0\xff");
-    }
+[TestFixture]
+public class OctetEncodingTests {
+  [Test]
+  public void TestEncodeTransfer7BitValidChar()
+  {
+    OctetEncoding.SevenBits.GetBytes("0004 append \"INBOX\" (\\Seen) {33}\r\n\x00\x20\x40\x60\x7f");
+  }
 
-    [Test]
-    public void TestEncodeTransfer7BitInvalidChar()
-    {
-      Assert.Throws<EncoderFallbackException>(() => OctetEncoding.SevenBits.GetBytes("\x20\x40\x60\x80"));
-    }
+  [Test]
+  public void TestEncodeTransfer8BitValidChar()
+  {
+    OctetEncoding.EightBits.GetBytes("0004 append \"INBOX\" (\\Seen) {33}\r\n\x00\x20\x40\x60\x80\xa0\xc0\xe0\xff");
+  }
 
-    [Test]
-    public void TestEncodeTransfer8BitInvalidChar()
-    {
-      Assert.Throws<EncoderFallbackException>(() => OctetEncoding.EightBits.GetBytes("INBOX.日本語"));
-    }
+  [Test]
+  public void TestEncodeTransfer7BitInvalidChar()
+  {
+    Assert.Throws<EncoderFallbackException>(() => OctetEncoding.SevenBits.GetBytes("\x20\x40\x60\x80"));
+  }
+
+  [Test]
+  public void TestEncodeTransfer8BitInvalidChar()
+  {
+    Assert.Throws<EncoderFallbackException>(() => OctetEncoding.EightBits.GetBytes("INBOX.日本語"));
   }
 }
