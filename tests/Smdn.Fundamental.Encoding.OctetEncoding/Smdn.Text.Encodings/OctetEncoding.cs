f495577b6ee99d6b61d71fd9120e437f0ea80ceb
smdn <smdn@smdn.jp>
2022-05-15 21:32:57 +0900

    rename test cases

--- tests/Smdn.Fundamental.Encoding.OctetEncoding/Smdn.Text.Encodings/OctetEncoding.cs
+++ tests/Smdn.Fundamental.Encoding.OctetEncoding/Smdn.Text.Encodings/OctetEncoding.cs
@@ -10,25 +10,25 @@ namespace Smdn.Text.Encodings;
 [TestFixture]
 public class OctetEncodingTests {
   [Test]
-  public void TestEncodeTransfer7BitValidChar()
+  public void GetBytes_SevenBits_ValidChar()
   {
     OctetEncoding.SevenBits.GetBytes("0004 append \"INBOX\" (\\Seen) {33}\r\n\x00\x20\x40\x60\x7f");
   }
 
   [Test]
-  public void TestEncodeTransfer8BitValidChar()
+  public void GetBytes_EightBits_ValidChar()
   {
     OctetEncoding.EightBits.GetBytes("0004 append \"INBOX\" (\\Seen) {33}\r\n\x00\x20\x40\x60\x80\xa0\xc0\xe0\xff");
   }
 
   [Test]
-  public void TestEncodeTransfer7BitInvalidChar()
+  public void GetBytes_SevenBits_InvalidChar()
   {
     Assert.Throws<EncoderFallbackException>(() => OctetEncoding.SevenBits.GetBytes("\x20\x40\x60\x80"));
   }
 
   [Test]
-  public void TestEncodeTransfer8BitInvalidChar()
+  public void GetBytes_EightBits_InvalidChar()
   {
     Assert.Throws<EncoderFallbackException>(() => OctetEncoding.EightBits.GetBytes("INBOX.日本語"));
   }
