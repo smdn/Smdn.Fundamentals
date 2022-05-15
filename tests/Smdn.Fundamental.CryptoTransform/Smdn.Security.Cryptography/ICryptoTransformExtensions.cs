2091ec2302e9b0b2dc469c21991180eddc62cd6c
smdn <smdn@smdn.jp>
2022-05-15 20:21:00 +0900

    use TestCaseSourceAttribute instead

--- tests/Smdn.Fundamental.CryptoTransform/Smdn.Security.Cryptography/ICryptoTransformExtensions.cs
+++ tests/Smdn.Fundamental.CryptoTransform/Smdn.Security.Cryptography/ICryptoTransformExtensions.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2010 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 using System;
+using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -45,25 +46,21 @@ public class ICryptoTransformExtensionsTests {
     );
   }
 
-  [Test]
-  public void TransformBytes_ArgumentException()
+  private static IEnumerable YieldTestCases_TransformBytes_ArgumentException()
   {
-    var buffer = new byte[] {0xff, 0xff, 0x61, 0x62, 0x63, 0x64, 0x65, 0xff, 0xff};
+    var input =  new byte[] {0xff, 0xff, 0x61, 0x62, 0x63, 0x64, 0x65, 0xff, 0xff};
 
-    using var transform = Smdn.Formats.Base64.CreateToBase64Transform();
-
-    Assert.Throws<ArgumentNullException>(() => ICryptoTransformExtensions.TransformBytes(null, buffer, 0, 9));
-
-    Assert.Throws<ArgumentNullException>(() => ICryptoTransformExtensions.TransformBytes(transform, null, 0, 9));
-
-    Assert.Throws<ArgumentOutOfRangeException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, -1, 10));
-
-    Assert.Throws<ArgumentOutOfRangeException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 10, -1));
-
-    Assert.Throws<ArgumentException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 1, 9));
-
-    Assert.Throws<ArgumentException>(() => ICryptoTransformExtensions.TransformBytes(transform, buffer, 9, 1));
+    yield return new object[] { typeof(ArgumentNullException), null, input, 0, 9 }; // transform null
+    yield return new object[] { typeof(ArgumentNullException), Smdn.Formats.Base64.CreateToBase64Transform(), null, 0, 9 }; // input null
+    yield return new object[] { typeof(ArgumentOutOfRangeException), Smdn.Formats.Base64.CreateToBase64Transform(), input, -1, 10 };
+    yield return new object[] { typeof(ArgumentOutOfRangeException), Smdn.Formats.Base64.CreateToBase64Transform(), input, 10, -1 };
+    yield return new object[] { typeof(ArgumentException), Smdn.Formats.Base64.CreateToBase64Transform(), input, 1, 9 };
+    yield return new object[] { typeof(ArgumentException), Smdn.Formats.Base64.CreateToBase64Transform(), input, 9, 1 };
   }
+
+  [TestCaseSource(nameof(YieldTestCases_TransformBytes_ArgumentException))]
+  public void TransformBytes_ArgumentException(Type expectedExceptionType, ICryptoTransform transform, byte[] input, int offset, int count)
+    => Assert.Throws(expectedExceptionType, () => ICryptoTransformExtensions.TransformBytes(transform, input, offset, count));
 
   private byte[] TransformByCryptoStream(HashAlgorithm algorithm, byte[] bytes)
   {
@@ -92,85 +89,82 @@ public class ICryptoTransformExtensionsTests {
   }
 
 #if NETFRAMEWORK || NETCOREAPP2_0_OR_GREATER || NET5_O_OR_GREATER
-  [Test]
-  public void TranformBytes_WithHashAlgorithm()
+  private static IEnumerable YieldTestCases_TranformBytes_WithHashAlgorithm()
   {
-    var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
-
-    var hashAlgorithms = new HashAlgorithm[] {
 #pragma warning disable CA5350, CA5351 // uses a weak/broken cryptographic algorithm
-      new HMACMD5(),
-      new HMACSHA512(),
-      MD5.Create(),
+    yield return new object[] { new HMACMD5() };
+    yield return new object[] { new HMACSHA512() };
+    yield return new object[] { MD5.Create() };
 #pragma warning disable SYSLIB0021
-      new SHA512Managed(),
+    yield return new object[] { new SHA512Managed() };
 #pragma warning restore SYSLIB0021
 #if NETFRAMEWORK
-      new RIPEMD160Managed(),
+    yield return new object[] { new RIPEMD160Managed() };
 #endif
 #pragma warning restore CA5350, CA5351
-    };
-
-    foreach (var hashAlgorithm in hashAlgorithms) {
-      hashAlgorithm.Initialize();
-
-      Assert.AreEqual(
-        hashAlgorithm.TransformBytes(bytes),
-        TransformByCryptoStream(hashAlgorithm, bytes),
-        "HashAlgorithm: {0}",
-        hashAlgorithm.GetType()
-      );
-    }
   }
 
-  [Test]
-  public void TranformBytes_WithSymmetricAlgorithm()
+  [TestCaseSource(nameof(YieldTestCases_TranformBytes_WithHashAlgorithm))]
+  public void TranformBytes_WithHashAlgorithm(HashAlgorithm hashAlgorithm)
   {
     var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
 
-    var symmetricAlgorithms = new SymmetricAlgorithm[] {
+    hashAlgorithm.Initialize();
+
+    Assert.AreEqual(
+      hashAlgorithm.TransformBytes(bytes),
+      TransformByCryptoStream(hashAlgorithm, bytes),
+      "HashAlgorithm: {0}",
+      hashAlgorithm.GetType()
+    );
+  }
+
+  private static IEnumerable YieldTestCases_TranformBytes_WithSymmetricAlgorithm()
+  {
 #pragma warning disable CA5350, CA5351 // uses a weak/broken cryptographic algorithm
 #pragma warning disable SYSLIB0022
-      Rijndael.Create(),
+    yield return new object[] { Rijndael.Create() };
 #pragma warning restore SYSLIB0022
-      DES.Create(),
-      TripleDES.Create(),
-      RC2.Create(),
+    yield return new object[] { DES.Create() };
+    yield return new object[] { TripleDES.Create() };
+    yield return new object[] { RC2.Create() };
 #pragma warning restore CA5350, CA5351
-    };
+  }
 
-    foreach (var symmetricAlgorithm in symmetricAlgorithms) {
-      var rng = RandomNumberGenerator.Create();
-      var nonce = new byte[symmetricAlgorithm.KeySize / 8];
+  [TestCaseSource(nameof(YieldTestCases_TranformBytes_WithSymmetricAlgorithm))]
+  public void TranformBytes_WithSymmetricAlgorithm(SymmetricAlgorithm symmetricAlgorithm)
+  {
+    var bytes = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
+    var rng = RandomNumberGenerator.Create();
+    var nonce = new byte[symmetricAlgorithm.KeySize / 8];
 
-      rng.GetBytes(nonce);
+    rng.GetBytes(nonce);
 
-      symmetricAlgorithm.Padding = PaddingMode.Zeros;
-      symmetricAlgorithm.Key = nonce;
-      symmetricAlgorithm.GenerateIV();
+    symmetricAlgorithm.Padding = PaddingMode.Zeros;
+    symmetricAlgorithm.Key = nonce;
+    symmetricAlgorithm.GenerateIV();
 
-      symmetricAlgorithm.Clear();
+    symmetricAlgorithm.Clear();
 
-      var encrypted = symmetricAlgorithm.CreateEncryptor().TransformBytes(bytes);
+    var encrypted = symmetricAlgorithm.CreateEncryptor().TransformBytes(bytes);
 
-      Assert.AreEqual(
-        BitConverter.ToString(TransformByCryptoStream(symmetricAlgorithm, bytes, true)),
-        BitConverter.ToString(encrypted),
-        "SymmetricAlgorithm (Encrypt): {0}",
-        symmetricAlgorithm.GetType()
-      );
+    Assert.AreEqual(
+      BitConverter.ToString(TransformByCryptoStream(symmetricAlgorithm, bytes, true)),
+      BitConverter.ToString(encrypted),
+      "SymmetricAlgorithm (Encrypt): {0}",
+      symmetricAlgorithm.GetType()
+    );
 
-      symmetricAlgorithm.Clear();
+    symmetricAlgorithm.Clear();
 
-      var decrypted = Encoding.ASCII.GetString(symmetricAlgorithm.CreateDecryptor().TransformBytes(encrypted));
+    var decrypted = Encoding.ASCII.GetString(symmetricAlgorithm.CreateDecryptor().TransformBytes(encrypted));
 
-      Assert.AreEqual(
-        Encoding.ASCII.GetString(TransformByCryptoStream(symmetricAlgorithm, encrypted, false)),
-        decrypted,
-        "SymmetricAlgorithm (Decrypt): {0}",
-        symmetricAlgorithm.GetType()
-      );
-    }
+    Assert.AreEqual(
+      Encoding.ASCII.GetString(TransformByCryptoStream(symmetricAlgorithm, encrypted, false)),
+      decrypted,
+      "SymmetricAlgorithm (Decrypt): {0}",
+      symmetricAlgorithm.GetType()
+    );
   }
 #endif
 }
