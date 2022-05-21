4d65d646373131f605b06ee7a95c7764f93fdf2a
smdn <smdn@smdn.jp>
2022-05-21 23:32:41 +0900

    add test cases

--- tests/Smdn.Fundamental.String.Replacement/Smdn/StringReplacementExtensions.cs
+++ tests/Smdn.Fundamental.String.Replacement/Smdn/StringReplacementExtensions.cs
@@ -42,7 +42,33 @@ public class StringReplacementExtensionsTests {
   }
 
   [Test]
-  public void TestReplace_Null()
+  public void TestReplace_InputEmpty()
+  {
+    Assert.AreEqual(string.Empty, string.Empty.Replace(new[] {'a', 'h', 'p', 'z'}, delegate(char c, string s, int i) {
+      return string.Format("{0}{1}", i, Char.ToUpper(c));
+    }), "replace chars");
+
+    Assert.AreEqual(string.Empty, string.Empty.Replace(new[] {"abc", "mno", "xyz"}, delegate(string matched, string s, int i) {
+      return string.Format("{0}{1}", i, matched.ToUpper());
+    }), "replace strings");
+  }
+
+  [Test]
+  public void TestReplace_OldValuesEmpty()
+  {
+    var str = "abcdefghijklmnopqrstuvwxyz";
+
+    Assert.AreEqual(str, str.Replace(new char[0], delegate(char c, string s, int i) {
+      return string.Format("{0}{1}", i, Char.ToUpper(c));
+    }), "replace chars");
+
+    Assert.AreEqual(str, str.Replace(new string[0], delegate(string matched, string s, int i) {
+      return string.Format("{0}{1}", i, matched.ToUpper());
+    }), "replace strings");
+  }
+
+  [Test]
+  public void TestReplace_ReplaceToNull()
   {
     var str = "abcdefghijklmnopqrstuvwxyz";
 
@@ -52,7 +78,7 @@ public class StringReplacementExtensionsTests {
   }
 
   [Test]
-  public void TestReplace_Empty()
+  public void TestReplace_ReplaceToEmpty()
   {
     var str = "abcdefghijklmnopqrstuvwxyz";
 
