017797abe795e78eee233c9d1c91bed5975edca3
smdn <smdn@smdn.jp>
2022-07-07 00:35:12 +0900

    add ParameterInfoExtensions.GetDeclaringProperty/GetDeclaringEvent

--- src/Smdn.Fundamental.Reflection/Smdn.Reflection/ParameterInfoExtensions.cs
+++ src/Smdn.Fundamental.Reflection/Smdn.Reflection/ParameterInfoExtensions.cs
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 using System;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Smdn.Reflection;
 
@@ -16,5 +18,48 @@ public static class ParameterInfoExtensions {
 #else
     return param == (param.Member as MethodInfo)?.ReturnParameter;
 #endif
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static BindingFlags GetBindingFlagsForAccessorOwner(MethodInfo accessor)
+    => (accessor.IsStatic ? BindingFlags.Static : BindingFlags.Instance) | BindingFlags.Public | BindingFlags.NonPublic;
+
+  public static PropertyInfo? GetDeclaringProperty(this ParameterInfo param)
+  {
+    if (param is null)
+      throw new ArgumentNullException(nameof(param));
+    if (param.Member is not MethodInfo accessor)
+      return null;
+    if (!accessor.IsSpecialName) // property accessor methods must have special name
+      return null;
+    if (accessor.DeclaringType is null)
+      return null;
+
+    var properties = accessor.DeclaringType.GetProperties(GetBindingFlagsForAccessorOwner(accessor));
+
+    return IsReturnParameter(param)
+      ? properties.FirstOrDefault(property => accessor == property.GetMethod)
+      : properties.FirstOrDefault(property => accessor == property.SetMethod);
+  }
+
+  public static EventInfo? GetDeclaringEvent(this ParameterInfo param)
+  {
+    if (param is null)
+      throw new ArgumentNullException(nameof(param));
+    if (IsReturnParameter(param)) // event accessor methods never have return value
+      return null;
+    if (!string.Equals("value", param.Name, StringComparison.Ordinal)) // parameter name of event accessor method must be `value`
+      return null;
+    if (param.Member is not MethodInfo accessor)
+      return null;
+    if (!accessor.IsSpecialName) // event accessor methods must have special name
+      return null;
+    if (accessor.DeclaringType is null)
+      return null;
+
+    return accessor
+      .DeclaringType
+      .GetEvents(GetBindingFlagsForAccessorOwner(accessor))
+      .FirstOrDefault(ev => accessor == ev.AddMethod || accessor == ev.RemoveMethod);
   }
 }
--- tests/Smdn.Fundamental.Reflection/Smdn.Reflection/ParameterInfoExtensions.cs
+++ tests/Smdn.Fundamental.Reflection/Smdn.Reflection/ParameterInfoExtensions.cs
@@ -3,6 +3,7 @@
 #pragma warning disable IDE0051, CS0067, CS8597
 
 using System;
+using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
 
@@ -10,14 +11,87 @@ namespace Smdn.Reflection;
 
 [TestFixture()]
 public class ParameterInfoExtensionsTests {
+  public abstract class AccessorParameterAttribute : Attribute {
+    public string DeclaringMemberName { get; }
+
+    protected AccessorParameterAttribute(string declaringMemberName)
+    {
+      DeclaringMemberName = declaringMemberName;
+    }
+
+    protected MemberInfo GetDeclaringMember(ParameterInfo attributeProvider)
+    {
+      var type = attributeProvider.Member.DeclaringType!;
+
+      return type.GetMember(
+        name: DeclaringMemberName,
+        bindingAttr: BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+      ).FirstOrDefault();
+    }
+  }
+
+  [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false)]
+  public class PropertyAccessorParameterAttribute : AccessorParameterAttribute
+  {
+    public PropertyAccessorParameterAttribute(string declaringMemberName)
+      :  base(declaringMemberName)
+    {
+    }
+
+    public PropertyInfo GetDeclaringProperty(ParameterInfo attributeProvider)
+      => GetDeclaringMember(attributeProvider) as PropertyInfo;
+  }
+
+  [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false)]
+  public class EventAccessorParameterAttribute : AccessorParameterAttribute
+  {
+    public EventAccessorParameterAttribute(string declaringMemberName)
+      :  base(declaringMemberName)
+    {
+    }
+
+    public EventInfo GetDeclaringEvent(ParameterInfo attributeProvider)
+      => GetDeclaringMember(attributeProvider) as EventInfo;
+  }
+
   class C {
     public C(int x) => throw new NotImplementedException();
     public int M(int x, string y) => throw new NotImplementedException();
     protected int MProtected(int x, string y) => throw new NotImplementedException();
     private int MPrivate(int x, string y) => throw new NotImplementedException();
     public void M(object z) => throw new NotImplementedException();
-    public int P { get; set; }
-    public event EventHandler E;
+
+    public static int MStatic(int x, string y) => throw new NotImplementedException();
+
+    public int P {
+      [return: PropertyAccessorParameter(nameof(P))] get;
+      [param: PropertyAccessorParameter(nameof(P))] set;
+    }
+
+    private int PPrivate {
+      [return: PropertyAccessorParameter(nameof(PPrivate))] get;
+      [param: PropertyAccessorParameter(nameof(PPrivate))] set;
+    }
+
+    public static int PStatic {
+      [return: PropertyAccessorParameter(nameof(PStatic))] get;
+      [param: PropertyAccessorParameter(nameof(PStatic))] set;
+    }
+
+    public event EventHandler E {
+      [param: EventAccessorParameter(nameof(E))] add => throw null;
+      [param: EventAccessorParameter(nameof(E))] remove => throw null;
+    }
+
+    private event EventHandler EPrivate {
+      [param: EventAccessorParameter(nameof(EPrivate))] add => throw null;
+      [param: EventAccessorParameter(nameof(EPrivate))] remove => throw null;
+    }
+
+    public static event EventHandler EStatic {
+      [param: EventAccessorParameter(nameof(EStatic))] add => throw null;
+      [param: EventAccessorParameter(nameof(EStatic))] remove => throw null;
+    }
   }
 
   private static System.Collections.IEnumerable YieldTestCases_IsReturnParameter()
@@ -60,4 +134,66 @@ public class ParameterInfoExtensionsTests {
   [TestCaseSource(nameof(YieldTestCases_IsReturnParameter))]
   public void IsReturnParameter(ParameterInfo para, bool expected)
     => Assert.AreEqual(expected, para.IsReturnParameter(), $"{para.Member} {para.Name}");
+
+  [TestCase]
+  public void IsReturnParameter_ArgumentNull()
+    => Assert.Throws<ArgumentNullException>(() => ((ParameterInfo)null!).IsReturnParameter());
+
+  private static System.Collections.IEnumerable YieldTestCases_GetDeclaringProperty()
+  {
+    foreach (var args in YieldTestCases_IsReturnParameter()) {
+      yield return new object[] { ((object[])args)![0]! };
+    }
+  }
+
+  [TestCaseSource(nameof(YieldTestCases_GetDeclaringProperty))]
+  public void GetDeclaringProperty(ParameterInfo para)
+  {
+    var property = para.GetDeclaringProperty();
+    var attr =
+#if NETFRAMEWORK
+      // ???
+      para.GetCustomAttributes(typeof(PropertyAccessorParameterAttribute), inherit: false).FirstOrDefault() as PropertyAccessorParameterAttribute;
+#else
+      para.GetCustomAttribute<PropertyAccessorParameterAttribute>();
+#endif
+
+    if (attr is null)
+      Assert.IsNull(property);
+    else
+      Assert.AreEqual(attr.GetDeclaringProperty(para), property);
+  }
+
+  [TestCase]
+  public void GetDeclaringProperty_ArgumentNull()
+    => Assert.Throws<ArgumentNullException>(() => ((ParameterInfo)null!).GetDeclaringProperty());
+
+  private static System.Collections.IEnumerable YieldTestCases_GetDeclaringEvent()
+  {
+    foreach (var args in YieldTestCases_IsReturnParameter()) {
+      yield return new object[] { ((object[])args)![0]! };
+    }
+  }
+
+  [TestCaseSource(nameof(YieldTestCases_GetDeclaringEvent))]
+  public void GetDeclaringEvent(ParameterInfo para)
+  {
+    var ev = para.GetDeclaringEvent();
+    var attr =
+#if NETFRAMEWORK
+      // ???
+      para.GetCustomAttributes(typeof(EventAccessorParameterAttribute), inherit: false).FirstOrDefault() as EventAccessorParameterAttribute;
+#else
+      para.GetCustomAttribute<EventAccessorParameterAttribute>();
+#endif
+
+    if (attr is null)
+      Assert.IsNull(ev);
+    else
+      Assert.AreEqual(attr.GetDeclaringEvent(para), ev);
+  }
+
+  [TestCase]
+  public void GetDeclaringEvent_ArgumentNull()
+    => Assert.Throws<ArgumentNullException>(() => ((ParameterInfo)null!).GetDeclaringEvent());
 }
