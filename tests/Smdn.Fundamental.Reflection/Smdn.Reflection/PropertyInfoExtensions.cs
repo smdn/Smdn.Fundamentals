08ac84e4e63514921e8d1190d01525bcfe2c5d90
smdn <smdn@smdn.jp>
2022-07-06 22:01:18 +0900

    add PropertyInfoExtensions.GetBackingField

--- tests/Smdn.Fundamental.Reflection/Smdn.Reflection/PropertyInfoExtensions.cs
+++ tests/Smdn.Fundamental.Reflection/Smdn.Reflection/PropertyInfoExtensions.cs
@@ -12,10 +12,27 @@ public class PropertyInfoExtensionsTests {
     public int P0 { get; init; }
     public int P1 { get; set; }
     public int P2 { get; } = 0;
+    public int P3 { get => 0; }
 
     public static int SP0 { get; } = 0;
     public static int SP1 { set { } }
     public static int SP2 { get; set; }
+  }
+
+  public abstract class CBase {
+    public virtual int PVirtual0 { get; }
+    public virtual int PVirtual1 { get; }
+    public virtual int PVirtual2 { get => 0; }
+    public abstract int PAbstract0 { get; }
+    public abstract int PAbstract1 { get; }
+  }
+
+  public class CEx : CBase {
+    public override int PVirtual0 { get; }
+    public override int PVirtual1 { get => 0; }
+    public override int PVirtual2 { get; }
+    public override int PAbstract0 { get; }
+    public override int PAbstract1 { get => 0; }
   }
 
   static class SC {
@@ -27,6 +44,9 @@ public class PropertyInfoExtensionsTests {
     public int P1 { get; set; }
     public int P2 { get => 0; }
     public readonly int P3 { get; init; }
+
+    public static int SP0 { get; set; }
+    public static int SP1 { get => 0; }
   }
 
   readonly struct ROS {
@@ -79,4 +99,41 @@ public class PropertyInfoExtensionsTests {
   [Test]
   public void IsSetMethodInitOnly_ArgumentNull()
     => Assert.Throws<ArgumentNullException>(() => ((PropertyInfo)null!).IsSetMethodInitOnly());
+
+  [TestCase(typeof(C), nameof(C.P0), true)]
+  [TestCase(typeof(C), nameof(C.P1), true)]
+  [TestCase(typeof(C), nameof(C.P2), true)]
+  [TestCase(typeof(C), nameof(C.P3), false)]
+  [TestCase(typeof(C), nameof(C.SP0), true)]
+  [TestCase(typeof(C), nameof(C.SP1), false)]
+  [TestCase(typeof(C), nameof(C.SP2), true)]
+  [TestCase(typeof(S), nameof(S.P0), true)]
+  [TestCase(typeof(S), nameof(S.P1), true)]
+  [TestCase(typeof(S), nameof(S.P2), false)]
+  [TestCase(typeof(S), nameof(S.P3), true)]
+  [TestCase(typeof(S), nameof(S.SP0), true)]
+  [TestCase(typeof(S), nameof(S.SP1), false)]
+  [TestCase(typeof(CBase), nameof(CBase.PVirtual0), true)]
+  [TestCase(typeof(CBase), nameof(CBase.PVirtual1), true)]
+  [TestCase(typeof(CBase), nameof(CBase.PVirtual2), false)]
+  [TestCase(typeof(CEx), nameof(CEx.PVirtual0), true)]
+  [TestCase(typeof(CEx), nameof(CEx.PVirtual1), false)]
+  [TestCase(typeof(CEx), nameof(CEx.PVirtual2), true)]
+  [TestCase(typeof(CBase), nameof(CBase.PAbstract0), false)]
+  [TestCase(typeof(CBase), nameof(CBase.PAbstract1), false)]
+  [TestCase(typeof(CEx), nameof(CEx.PAbstract0), true)]
+  [TestCase(typeof(CEx), nameof(CEx.PAbstract1), false)]
+  public void GetBackingField(Type type, string propertyName, bool hasBackingField)
+  {
+    var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+    if (hasBackingField)
+      Assert.IsNotNull(property!.GetBackingField());
+    else
+      Assert.IsNull(property!.GetBackingField());
+  }
+
+  [Test]
+  public void GetBackingField_ArgumentNull()
+    => Assert.Throws<ArgumentNullException>(() => ((PropertyInfo)null!).GetBackingField());
 }
