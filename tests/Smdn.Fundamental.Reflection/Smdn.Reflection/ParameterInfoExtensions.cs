e5decd12cd46e1ac65c6b3fd1630842a4c3c6dff
smdn <smdn@smdn.jp>
2022-07-06 23:23:23 +0900

    add test cases

--- tests/Smdn.Fundamental.Reflection/Smdn.Reflection/ParameterInfoExtensions.cs
+++ tests/Smdn.Fundamental.Reflection/Smdn.Reflection/ParameterInfoExtensions.cs
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#pragma warning disable CS0067
+
 using System;
 using System.Reflection;
 using NUnit.Framework;
@@ -12,6 +14,8 @@ public class ParameterInfoExtensionsTests {
     public C(int x) => throw new NotImplementedException();
     public int M(int x, string y) => throw new NotImplementedException();
     public void M(object z) => throw new NotImplementedException();
+    public int P { get; set; }
+    public event EventHandler E;
   }
 
   private static System.Collections.IEnumerable YieldTestCases_IsReturnParameter()
@@ -28,6 +32,20 @@ public class ParameterInfoExtensionsTests {
       foreach (var p in ctor.GetParameters()) {
         yield return new object[] { p, false };
       }
+    }
+
+    foreach (var property in typeof(C).GetProperties()) {
+      if (property.SetMethod is not null)
+        yield return new object[] { property.SetMethod.GetParameters()[0], false };
+      if (property.GetMethod is not null)
+        yield return new object[] { property.GetMethod.ReturnParameter, true };
+    }
+
+    foreach (var ev in typeof(C).GetEvents()) {
+      if (ev.AddMethod is not null)
+        yield return new object[] { ev.AddMethod.GetParameters()[0], false };
+      if (ev.RemoveMethod is not null)
+        yield return new object[] { ev.RemoveMethod.GetParameters()[0], false };
     }
   }
 
