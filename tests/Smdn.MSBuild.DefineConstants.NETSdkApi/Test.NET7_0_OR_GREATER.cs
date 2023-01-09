12b2754708227f0d7ce475823e21ded8e9eba2a9
smdn <smdn@smdn.jp>
2023-01-09 14:06:35 +0900

    add tests

--- tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET7_0_OR_GREATER.cs
+++ tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET7_0_OR_GREATER.cs
@@ -1,7 +1,11 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 #if NET7_0_OR_GREATER
-  #if !SYSTEM_INUMBER
-    #error "SYSTEM_INUMBER is not defined"
+  #if !GENERIC_MATH_INTERFACES
+    #error "GENERIC_MATH_INTERFACES is not defined"
+  #endif
+
+  #if !SYSTEM_NUMERICS_INUMBER
+    #error "SYSTEM_NUMERICS_INUMBER is not defined"
   #endif
 #endif
--- tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NETSTANDARD2_1_OR_GREATER+NETCOREAPP3_0_OR_GREATER+NET5_0_OR_GREATER.cs
+++ tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NETSTANDARD2_1_OR_GREATER+NETCOREAPP3_0_OR_GREATER+NET5_0_OR_GREATER.cs
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+  #if !NULL_STATE_STATIC_ANALYSIS_ATTRIBUTES
+    #error "NULL_STATE_STATIC_ANALYSIS_ATTRIBUTES is not defined"
+  #endif
+
   #if !SYSTEM_INDEX
     #error "SYSTEM_INDEX is not defined"
   #endif
