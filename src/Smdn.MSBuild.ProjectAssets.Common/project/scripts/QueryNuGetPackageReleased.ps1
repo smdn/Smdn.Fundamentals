63d107893df9c47c7908dea23e7356ff363d2d87
smdn <smdn@smdn.jp>
2023-01-20 01:53:20 +0900

    use modularized script

--- src/Smdn.MSBuild.ProjectAssets.Common/project/scripts/QueryNuGetPackageReleased.ps1
+++ src/Smdn.MSBuild.ProjectAssets.Common/project/scripts/QueryNuGetPackageReleased.ps1
@@ -24,29 +24,7 @@ Param(
   [string]$PackageSource = 'https://api.nuget.org/v3/index.json'
 )
 
-function Get-NuGetResourceBaseAddress {
-  [OutputType([string])]
-  Param(
-    [Parameter(Mandatory = $true)][string]$resource_type,
-    [Parameter(Mandatory = $true)][string]$source_path
-  )
-
-  $get_service_index_response = Invoke-WebRequest `
-    -Method 'Get' `
-    -SkipHttpErrorCheck `
-    -URI $source_path
-
-  if ($get_service_index_response.StatusCode -ne 200) {
-    $get_service_index_response | Write-Error
-    throw "Failed to GET resource base address for ${resource_type} (StatusCode: $($get_service_index_response.StatusCode))"
-  }
-
-  $service_index = $get_service_index_response.Content | ConvertFrom-Json
-
-  return $service_index.resources | `
-    Where-Object '@type' -eq $resource_type | `
-    Select-Object -ExpandProperty '@id'
-}
+Import-Module "$PSScriptRoot\psmodules\NuGet.ServiceIndex\" -Function 'Get-NuGetResourceBaseAddress'
 
 function Get-NuGetPackageManifestStatus {
   [OutputType([bool])]
