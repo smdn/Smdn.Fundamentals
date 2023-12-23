87ab9faf074eb98693725a4f6a5feb38b6f3e1ba
smdn <smdn@smdn.jp>
2023-12-24 00:38:03 +0900

    fix implementation of IAdditiveIdentity and IMultiplicativeIdentity

--- src/Smdn.Fundamental.UInt24n/UInt24nTemplates/TUInt24n.IAdditiveIdentity.cs
+++ src/Smdn.Fundamental.UInt24n/UInt24nTemplates/TUInt24n.IAdditiveIdentity.cs
@@ -7,18 +7,8 @@ using System.Numerics;
 namespace Smdn;
 
 #pragma warning disable IDE0040
-partial struct TUInt24n
-#if false
-  :
-  IAdditiveIdentity<TUInt24n, TIntWide>,
-  IAdditiveIdentity<TUInt24n, TUIntWide>
-#endif
-{
+partial struct TUInt24n : IAdditiveIdentity<TUInt24n, TUInt24n> {
 #pragma warning restore IDE0040
   static TUInt24n IAdditiveIdentity<TUInt24n, TUInt24n>.AdditiveIdentity => Zero;
-#if false
-  static TIntWide IAdditiveIdentity<TUInt24n, TIntWide>.AdditiveIdentity => (TIntWide)0;
-  static TUIntWide IAdditiveIdentity<TUInt24n, TUIntWide>.AdditiveIdentity => (TUIntWide)0u;
-#endif
 }
 #endif
--- src/Smdn.Fundamental.UInt24n/UInt24nTemplates/TUInt24n.IMultiplicativeIdentity.cs
+++ src/Smdn.Fundamental.UInt24n/UInt24nTemplates/TUInt24n.IMultiplicativeIdentity.cs
@@ -7,18 +7,8 @@ using System.Numerics;
 namespace Smdn;
 
 #pragma warning disable IDE0040
-partial struct TUInt24n
-#if false
-  :
-  IMultiplicativeIdentity<TUInt24n, TIntWide>,
-  IMultiplicativeIdentity<TUInt24n, TUIntWide>
-#endif
-{
+partial struct TUInt24n : IMultiplicativeIdentity<TUInt24n, TUInt24n> {
 #pragma warning restore IDE0040
   static TUInt24n IMultiplicativeIdentity<TUInt24n, TUInt24n>.MultiplicativeIdentity => One;
-#if false
-  static TIntWide IMultiplicativeIdentity<TUInt24n, TIntWide>.MultiplicativeIdentity => (TIntWide)1;
-  static TUIntWide IMultiplicativeIdentity<TUInt24n, TUIntWide>.MultiplicativeIdentity => (TUIntWide)1u;
-#endif
 }
 #endif
