14af364738b7d1f909e304637b8b2565a4901859
smdn <smdn@smdn.jp>
2024-02-07 07:15:31 +0900

    improve CustomAttributeTypedArgumentExtensions.GetTypedValue() to work in reflection-only context

--- src/Smdn.Fundamental.Reflection/Smdn.Reflection.Attributes/CustomAttributeTypedArgumentExtensions.cs
+++ src/Smdn.Fundamental.Reflection/Smdn.Reflection.Attributes/CustomAttributeTypedArgumentExtensions.cs
@@ -8,7 +8,33 @@ namespace Smdn.Reflection.Attributes;
 public static class CustomAttributeTypedArgumentExtensions {
   // CustomAttributeTypedArgument.Value holds an underlying typed value if the ArgumentType is enum
   public static object? GetTypedValue(this CustomAttributeTypedArgument typedArg)
-    => typedArg.ArgumentType.IsEnum && typedArg.Value is not null
-      ? Enum.ToObject(typedArg.ArgumentType, typedArg.Value)
+    => typedArg.ArgumentType.IsEnum
+      ? GetTypedEnumValue(typedArg)
       : typedArg.Value;
+
+  private static object? GetTypedEnumValue(CustomAttributeTypedArgument typedArg)
+  {
+    if (typedArg.Value is null)
+      return null;
+
+    // try to convert a raw value to corresponding enum value
+    try {
+      return Enum.ToObject(enumType: typedArg.ArgumentType, typedArg.Value);
+    }
+    catch (ArgumentException ex) when (string.Equals(ex.ParamName, "enumType", StringComparison.Ordinal)) {
+      var isRuntimeType = string.Equals(
+        typedArg.ArgumentType.GetType().FullName,
+        "System.RuntimeType",
+        StringComparison.Ordinal
+      );
+
+      if (isRuntimeType)
+        throw;
+
+      // return the value as it is without conversion to enum since
+      // the type may have been loaded in a reflection-only context and
+      // the value may not be able to convert to the acutual enum type
+      return typedArg.Value;
+    }
+  }
 }
