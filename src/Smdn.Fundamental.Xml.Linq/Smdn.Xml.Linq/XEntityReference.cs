7d02cd68fe07f86f964978353645fa527cc57d23
smdn <smdn@smdn.jp>
2024-03-11 00:16:14 +0900

    fix XEntityReference to support async operation

--- src/Smdn.Fundamental.Xml.Linq/Smdn.Xml.Linq/XEntityReference.cs
+++ src/Smdn.Fundamental.Xml.Linq/Smdn.Xml.Linq/XEntityReference.cs
@@ -1,6 +1,14 @@
 // SPDX-FileCopyrightText: 2018 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER || NET5_0_OR_GREATER
+#define SYSTEM_XML_LINQ_XNODE_WRITETOASYNC
+#endif
+
 using System;
+#if SYSTEM_XML_LINQ_XNODE_WRITETOASYNC
+using System.Threading;
+using System.Threading.Tasks;
+#endif
 using System.Xml;
 using System.Xml.Linq;
 
@@ -14,4 +22,9 @@ public class XEntityReference : XText {
 
   public override void WriteTo(XmlWriter writer)
     => (writer ?? throw new ArgumentNullException(nameof(writer))).WriteEntityRef(Value);
+
+#if SYSTEM_XML_LINQ_XNODE_WRITETOASYNC
+  public override Task WriteToAsync(XmlWriter writer, CancellationToken cancellationToken)
+    => (writer ?? throw new ArgumentNullException(nameof(writer))).WriteEntityRefAsync(Value);
+#endif
 }
--- tests/Smdn.Fundamental.Xml.Linq/Smdn.Xml.Linq/XEntityReference.cs
+++ tests/Smdn.Fundamental.Xml.Linq/Smdn.Xml.Linq/XEntityReference.cs
@@ -1,8 +1,15 @@
 // SPDX-FileCopyrightText: 2022 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER || NET5_0_OR_GREATER
+#define SYSTEM_XML_LINQ_XNODE_WRITETOASYNC
+#endif
+
 using System;
 using System.IO;
 using System.Text;
+#if SYSTEM_XML_LINQ_XNODE_WRITETOASYNC
+using System.Threading.Tasks;
+#endif
 using System.Xml;
 using NUnit.Framework;
 
@@ -25,4 +32,23 @@ public class XEntityReferenceTests {
 
     Assert.That(sb.ToString(), Is.EqualTo("&copy;"));
   }
+
+#if SYSTEM_XML_LINQ_XNODE_WRITETOASYNC
+  [Test]
+  public async Task TestWriteToAsync()
+  {
+    var er = new XEntityReference("copy");
+    var sb = new StringBuilder();
+    var settings = new XmlWriterSettings() {
+      Async = true,
+      ConformanceLevel = ConformanceLevel.Fragment,
+    };
+
+    await using (var writer = XmlWriter.Create(new StringWriter(sb), settings)) {
+      await er.WriteToAsync(writer, default).ConfigureAwait(false);
+    }
+
+    Assert.That(sb.ToString(), Is.EqualTo("&copy;"));
+  }
+#endif
 }
