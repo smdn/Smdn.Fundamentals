d57e9c86e920211ef49a64a08edaed762de64e8d
smdn <smdn@smdn.jp>
2024-11-04 23:49:30 +0900

    add .NET 9 APIs

---
+++ tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET9_0_OR_GREATER.cs
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2023 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+#if NET9_0_OR_GREATER
+  #if !SYSTEM_LINQ_ENUMERABLE_INDEX
+    #error "SYSTEM_LINQ_ENUMERABLE_INDEX is not defined"
+  #endif
+#endif
