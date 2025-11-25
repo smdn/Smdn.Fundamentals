08852814ce04eb9cb4bb12a8a30789fc451aeb86
smdn <smdn@smdn.jp>
2025-11-25 21:57:29 +0900

    add CompareOptions.NumericOrdering

--- tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET10_0_OR_GREATER.cs
+++ tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET10_0_OR_GREATER.cs
@@ -1,4 +1,7 @@
 // SPDX-FileCopyrightText: 2025 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 #if NET10_0_OR_GREATER
+  #if !SYSTEM_GLOBALIZATION_COMPAREOPTIONS_NUMERICORDERING
+    #error "SYSTEM_GLOBALIZATION_COMPAREOPTIONS_NUMERICORDERING is not defined"
+  #endif
 #endif
