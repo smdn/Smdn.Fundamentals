f6792867dcf1487aa9e91371da6684f792770dc1
smdn <smdn@smdn.jp>
2026-08-30 18:45:38 +0900

    add System.Type.GetNullableUnderlyingType

--- tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET11_0_OR_GREATER.cs
+++ tests/Smdn.MSBuild.DefineConstants.NETSdkApi/Test.NET11_0_OR_GREATER.cs
@@ -1,4 +1,7 @@
 // SPDX-FileCopyrightText: 2026 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
 #if NET11_0_OR_GREATER
+  #if !SYSTEM_TYPE_GETNULLABLEUNDERLYINGTYPE
+    #error "SYSTEM_TYPE_GETNULLABLEUNDERLYINGTYPE is not defined"
+  #endif
 #endif
